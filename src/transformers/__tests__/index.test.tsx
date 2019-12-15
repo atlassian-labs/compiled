b229dc749e4614bb8d9194c8de340a82f10c8f8a
Michael Dougall <mdougall@atlassian.com>
2019-12-15 20:27:15 +1100

    chore(transformer): adds var test

--- src/transformers/__tests__/index.test.tsx
+++ src/transformers/__tests__/index.test.tsx
@@ -3,16 +3,34 @@ import rootTransformer from '../index';
 import pkg from '../../../package.json';
 
 describe('root transformer', () => {
-  it('should not blow up when transforming', () => {
+  it('should not blow up when transforming with const', () => {
     const transformer = rootTransformer({});
 
     expect(() => {
       ts.transpileModule(
-        `/** @jsx jsx */
+        `
+          /** @jsx jsx */
           import { jsx } from '${pkg.name}';
-
           const MyComponent = () => <div css={{ fontSize: '20px' }}>hello world</div>
-      `,
+        `,
+        {
+          transformers: { before: transformer },
+          compilerOptions: { module: ts.ModuleKind.ESNext, jsx: ts.JsxEmit.React },
+        }
+      );
+    }).not.toThrow();
+  });
+
+  it('should not blow up when transforming with var', () => {
+    const transformer = rootTransformer({});
+
+    expect(() => {
+      ts.transpileModule(
+        `
+          /** @jsx jsx */
+          import { jsx } from '${pkg.name}';
+          var MyComponent = () => <div css={{ fontSize: '20px' }}>hello world</div>
+        `,
         {
           transformers: { before: transformer },
           compilerOptions: { module: ts.ModuleKind.ESNext, jsx: ts.JsxEmit.React },
