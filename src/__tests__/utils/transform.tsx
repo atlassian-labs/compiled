89f781f8874f4dc56320ee0b56b0200288d37f09
Michael Dougall <mdougall@atlassian.com>
2019-12-17 16:00:46 +1100

    chore(tests): moves transform test helpers into util

---
+++ src/__tests__/utils/transform.tsx
@@ -0,0 +1,73 @@
+import * as ts from 'typescript';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const printer = ts.createPrinter();
+
+type ProgramTransformer = (program: ts.Program) => ts.TransformerFactory<ts.SourceFile>;
+
+/**
+ * This creates a full project which will resolve all modules.
+ * Only use this when wanting to test imports tbh. It's slow.
+ */
+export const createFullTransform = (programTransformer: ProgramTransformer, dir: string) => (
+  ...sources: string[]
+): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.rmdirSync(`${dir}/.tmp`, { recursive: true });
+    } catch {}
+
+    fs.mkdirSync(`${dir}/.tmp`);
+    const files: string[] = [];
+    sources.forEach((source, index) => {
+      const filename = index === 0 ? 'index.tsx' : `${index}.tsx`;
+      const filepath = path.resolve(`${dir}/.tmp/${filename}`);
+      files.push(filepath);
+      fs.writeFileSync(filepath, source);
+    });
+
+    const [rootFile] = files;
+    const config: ts.CompilerOptions = {
+      jsx: ts.JsxEmit.Preserve,
+      module: ts.ModuleKind.ESNext,
+      suppressImplicitAnyIndexErrors: true,
+      target: ts.ScriptTarget.ESNext,
+      // Uncomment this if shit isn't working.
+      // noEmitOnError: true,
+    };
+    const compilerHost = ts.createCompilerHost(config, true);
+    const program = ts.createProgram([rootFile], config, compilerHost);
+
+    const { emitSkipped, diagnostics, emittedFiles } = program.emit(
+      undefined,
+      (filename, data) => {
+        if (filename.endsWith('index.jsx')) {
+          resolve(data);
+        }
+      },
+      undefined,
+      false,
+      {
+        before: [programTransformer(program)],
+      }
+    );
+
+    if (emitSkipped) {
+      return reject(new Error(diagnostics.map(diagnostic => diagnostic.messageText).join('\n')));
+    }
+
+    if (!emittedFiles) {
+      return reject(new Error('Nothing was emitted'));
+    }
+  });
+};
+
+export const createTransform = (programTransformer: ProgramTransformer) => (
+  source: string
+): string => {
+  const transformer = programTransformer({} as any);
+  const sourceFile = ts.createSourceFile('index.tsx', source, ts.ScriptTarget.Latest);
+  const actual = ts.transform(sourceFile, [transformer]).transformed[0];
+  return printer.printFile(actual).toString();
+};
--- src/ts-transformer/css-prop/__tests__/index.test.tsx
+++ src/ts-transformer/css-prop/__tests__/index.test.tsx
@@ -1,74 +1,11 @@
-import * as ts from 'typescript';
-import * as fs from 'fs';
-import * as path from 'path';
 import cssPropTransformer from '../index';
 import pkg from '../../../../package.json';
+import { createFullTransform, createTransform } from '../../../__tests__/utils/transform';
 
 jest.mock('../../utils/identifiers');
 
-const printer = ts.createPrinter();
-
-/**
- * This creates a full project which will resolve all modules.
- * Only use this when wanting to test imports tbh. It's slow.
- */
-const fullTransform = (...sources: string[]): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    try {
-      fs.rmdirSync(`${__dirname}/.tmp`, { recursive: true });
-    } catch {}
-
-    fs.mkdirSync(`${__dirname}/.tmp`);
-    const files: string[] = [];
-    sources.forEach((source, index) => {
-      const filename = index === 0 ? 'index.tsx' : `${index}.tsx`;
-      const filepath = path.resolve(`${__dirname}/.tmp/${filename}`);
-      files.push(filepath);
-      fs.writeFileSync(filepath, source);
-    });
-
-    const [rootFile] = files;
-    const config: ts.CompilerOptions = {
-      jsx: ts.JsxEmit.Preserve,
-      module: ts.ModuleKind.ESNext,
-      suppressImplicitAnyIndexErrors: true,
-      target: ts.ScriptTarget.ESNext,
-      // Uncomment this if shit isn't working.
-      // noEmitOnError: true,
-    };
-    const compilerHost = ts.createCompilerHost(config, true);
-    const program = ts.createProgram([rootFile], config, compilerHost);
-
-    const { emitSkipped, diagnostics, emittedFiles } = program.emit(
-      undefined,
-      (filename, data) => {
-        if (filename.endsWith('index.jsx')) {
-          resolve(data);
-        }
-      },
-      undefined,
-      false,
-      {
-        before: [cssPropTransformer(program)],
-      }
-    );
-
-    if (emitSkipped) {
-      return reject(new Error(diagnostics.map(diagnostic => diagnostic.messageText).join('\n')));
-    }
-
-    if (!emittedFiles) {
-      return reject(new Error('Nothing was emitted'));
-    }
-  });
-};
-
-const transform = (source: string): string => {
-  const transformer = cssPropTransformer({} as any);
-  const sourceFile = ts.createSourceFile('index.tsx', source, ts.ScriptTarget.Latest);
-  const actual = ts.transform(sourceFile, [transformer]).transformed[0];
-  return printer.printFile(actual).toString();
-};
+const transform = createTransform(cssPropTransformer);
+const fullTransform = createFullTransform(cssPropTransformer, __dirname);
 
 describe('css prop transformer', () => {
   it('should replace css prop with class name', () => {
