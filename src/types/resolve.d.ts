538882bf1c3ea2bc6bfb52981e2fd6eb0d0be3fb
Mario Nebl <mnebl@atlassian.com>
2019-12-23 20:34:10 +1100

    chore: remove obsolete augmentation

--- src/types/resolve.d.ts
+++
@@ -1,8 +0,0 @@
-import resolve from 'resolve';
-
-declare module 'resolve' {
-  export interface SyncOpts extends resolve.Opts {
-    /** function to synchronously test whether a directory exists */
-    isDirectory?: (directory: string) => boolean;
-  }
-}
