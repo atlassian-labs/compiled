e823cb76c462f559f40f591785ffd8b9c46cf63b
Michael Dougall <mdougall@atlassian.com>
2020-01-11 10:22:42 +1100

    chore: adds media query examples

---
+++ examples/media-queries.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { styled } from '../src';
+
+export default {
+  title: 'media queries',
+};
+
+const ResponsiveStyledObjectLiteral = styled.div({
+  color: 'blue',
+  fontSize: 50,
+  '@media screen and (min-width: 800px)': {
+    color: 'red',
+    fontSize: 30,
+  },
+});
+
+const ResponsiveStyledTemplateLiteral = styled.div`
+  color: blue;
+  font-size: 50px;
+
+  @media screen and (min-width: 800px) {
+    color: red;
+    font-size: 30px;
+  }
+`;
+
+export const objectLiteral = () => (
+  <ResponsiveStyledObjectLiteral>hello world</ResponsiveStyledObjectLiteral>
+);
+
+export const templateLiteral = () => (
+  <ResponsiveStyledTemplateLiteral>hello world</ResponsiveStyledTemplateLiteral>
+);
