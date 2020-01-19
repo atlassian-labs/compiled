b390519e8bcab58dc01a1fa746e7d68d7ae4500e
Michael Dougall <mdougall@atlassian.com>
2020-01-19 21:29:51 +1100

    get it WORKING

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,9 @@ module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/src/**/*.test.ts*'],
   setupFilesAfterEnv: ['jest-extended', './test/setup.tsx'],
-  // moduleNameMapper: {
-  //   '^@compiled/css-in-js$': '<rootDir>/src/index.tsx',
-  // },
+  moduleNameMapper: {
+    '^@compiled/css-in-js$': '<rootDir>/packages/css-in-js/src/index.tsx',
+  },
   globals: {
     'ts-jest': {
       compiler: 'ttypescript',
