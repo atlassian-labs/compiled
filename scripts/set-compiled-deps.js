8ce856969075d93af93ce6a9234b2c97d69c15b4
madou <laheen@gmail.com>
2020-03-29 08:06:26 +1100

    chore: adds logging to version script

--- scripts/set-compiled-deps.js
+++ scripts/set-compiled-deps.js
@@ -7,11 +7,15 @@ const fs = require('fs');
 async function setDependencyVersions(version) {
   const pkgPath = `${process.cwd()}/package.json`;
   const pkgJson = require(pkgPath);
+
+  console.log(`👉 found workspace pkg ${pkgJson.name}`);
+
   let changed = false;
 
   const mappedDependencies = Object.entries(pkgJson.dependencies || {}).map(([key, value]) => {
     if (key.startsWith('@compiled')) {
       changed = true;
+      console.log(`👉 setting ${key} to ${pkgJson.version}`);
       return [key, version.replace('v', '')];
     }
 
@@ -21,6 +25,9 @@ async function setDependencyVersions(version) {
   if (changed) {
     pkgJson.dependencies = Object.fromEntries(mappedDependencies);
     fs.writeFileSync(pkgPath, JSON.stringify(pkgJson));
+    console.log(`👉 persisting`);
+  } else {
+    console.log(`👉 nothing to do`);
   }
 }
 
