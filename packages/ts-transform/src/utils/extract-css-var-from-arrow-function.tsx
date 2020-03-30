bcb4405bcf39abdfba7026c109b6ec9de3f2165b
madou <laheen@gmail.com>
2020-03-31 08:27:07 +1100

    fix(styled): always pass the entire expression from a styled function

--- packages/ts-transform/src/utils/extract-css-var-from-arrow-function.tsx
+++ packages/ts-transform/src/utils/extract-css-var-from-arrow-function.tsx
@@ -1,38 +1,15 @@
 import * as ts from 'typescript';
 import { CssVariableExpressions } from '../types';
-import { nextCssVariableName } from './identifiers';
-import { createNodeError } from './ast-node';
+import { hash } from './sequential-chars';
 
-/**
- * This expects a simple arrow function like the following:
- * `props => props.color`
- */
 export const extractCssVarFromArrowFunction = (
   node: ts.ArrowFunction,
   _: ts.TransformationContext
 ): CssVariableExpressions => {
-  let expression: ts.Expression = ts.createIdentifier('');
-  let name: string = '';
-  const body = ts.isParenthesizedExpression(node.body) ? node.body.expression : node.body;
+  const body: ts.Expression = (ts.isParenthesizedExpression(node.body)
+    ? node.body.expression
+    : node.body) as any;
+  const cssVariableName = hash(body.getText());
 
-  if (ts.isPropertyAccessExpression(body)) {
-    // We are returning a property immediately.
-    name = body.name.escapedText.toString();
-    // This changes props.color into an identifier instead.
-    // Technically this isn't right - we'll have to fix it sometime.
-    expression = ts.createIdentifier(body.getText());
-  } else if (ts.isTemplateExpression(body)) {
-    // TODO: Handle multiple expressions.
-    body.templateSpans.forEach(span => {
-      if (ts.isPropertyAccessExpression(span.expression)) {
-        name = span.expression.name.escapedText.toString();
-      }
-    });
-
-    expression = body;
-  } else {
-    throw createNodeError('unsupported arrow function body', node);
-  }
-
-  return { expression, name: `--${name}-${nextCssVariableName()}` };
+  return { expression: body, name: `--var-${cssVariableName}` };
 };
