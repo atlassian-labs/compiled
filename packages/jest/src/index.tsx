abfbb0501ae707d6c83ddbd4a77317c9a8711df5
madou <laheen@gmail.com>
2020-03-31 18:40:14 +1100

    chore: fix tests

--- packages/jest/src/index.tsx
+++ packages/jest/src/index.tsx
@@ -1,1 +1,10 @@
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toHaveCompiledCss(properties: { [key: string]: string }): R;
+      toHaveCompiledCss(property: string, value: string): R;
+    }
+  }
+}
+
 export { toHaveCompiledCss } from './matchers';
