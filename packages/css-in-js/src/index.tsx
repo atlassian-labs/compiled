4c723df29883150305bbb2be3413efe4d40e7951
madou <laheen@gmail.com>
2020-04-05 09:12:35 +1000

    fix: fixes jsx typing

--- packages/css-in-js/src/index.tsx
+++ packages/css-in-js/src/index.tsx
@@ -1,3 +1,21 @@
+import { CSSProperties } from 'react';
+
 export { default as Style } from '@compiled/style';
 export { styled } from './styled';
 export { ClassNames } from './class-names';
+
+export type CSSProps = CSSProperties;
+
+declare module 'react' {
+  interface DOMAttributes<T> {
+    css?: CSSProps | { [key: string]: CSSProps } | string;
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes {
+      css?: CSSProps;
+    }
+  }
+}
