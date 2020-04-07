e6c112b469130c4f54215937bfb9e2f7140499d5
madou <laheen@gmail.com>
2020-04-07 18:56:05 +1000

    chore: add perf addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
+import { withPerformance } from 'storybook-addon-performance';
+
+addDecorator(withPerformance);
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../examples', false, /\.tsx$/), module);
