62be1e2112ba27b4de79dce23292bc6df3d937f7
DOUGES <madou@users.noreply.github.com>
2020-04-16 18:48:12 +1000

    Selector typing fix (#142)
    * fix: fixes css prop typing
    
    * fix: consolidates all typing for styles

--- packages/css-in-js/src/types.tsx
+++ packages/css-in-js/src/types.tsx
@@ -1,4 +1,21 @@
-export type Unused = any;
+import * as CSS from 'csstype';
+
+/**
+ * Typing for the interpolations.
+ */
+export type TemplateInterpolations = string | number;
+
+export type CSSProps<TValue = void> = CSS.Properties<string | number | TValue>;
+
+export type AnyKeyCssProps<TValue> = {
+  [key: string]: AnyKeyCssProps<TValue> | CSSProps<TValue> | TValue;
+};
+
+export type CssFunction<TValue = void> =
+  | CSSProps<TValue>
+  | AnyKeyCssProps<TValue>
+  | TemplateStringsArray
+  | string;
 
 declare global {
   namespace jest {
