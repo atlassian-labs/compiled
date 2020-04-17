a966f00c9fdb085cd5b1ee144b1fa440a12626df
Daniel Del Core <daniel.delcore.92@gmail.com>
2020-04-17 13:03:08 +1000

    refactor: jest mathcer now supports multiple style tags

--- packages/jest/src/matchers.tsx
+++ packages/jest/src/matchers.tsx
@@ -4,6 +4,17 @@ const kebabCase = (str: string) =>
     .replace(/\s+/g, '-')
     .toLowerCase();
 
+const mapProperties = (properties: Record<string, any>) =>
+  Object.keys(properties).map(property => `${kebabCase(property)}:${properties[property]}`);
+
+const getMountedProperties = () =>
+  Array.from(document.styleSheets)
+    .map(sheet =>
+      // @ts-ignore
+      sheet.cssRules.map((rule: CSSRule) => rule.style.cssText)
+    )
+    .join(' ');
+
 export function toHaveCompiledCss(
   this: jest.MatcherUtils,
   element: HTMLElement,
@@ -11,77 +22,56 @@ export function toHaveCompiledCss(
 ): jest.CustomMatcherResult {
   const [property, value] = args;
   const properties = typeof property === 'string' ? { [property]: value } : property;
-  let styleElement = element.parentElement && element.parentElement.querySelector('style');
+  const inlineStyleTag = element.parentElement && element.parentElement.querySelector('style');
+  const styleElements: HTMLStyleElement[] =
+    inlineStyleTag != null ? [inlineStyleTag] : Array.from(document.head.querySelectorAll('style'));
 
-  if (!styleElement) {
-    // There wasn't a style element found within - let's check the head for it instead.
-    const styleElements = Array.from(document.head.querySelectorAll('style'));
-    for (const tag of styleElements) {
-      if (tag.innerHTML.includes(element.className)) {
-        styleElement = tag as HTMLStyleElement;
-        break;
-      }
-    }
-  }
-
-  if (!styleElement) {
+  if (!styleElements) {
     return {
       pass: false,
       message: () => 'pairing style element was not found',
     };
   }
 
-  // This is a hack to get ahold of the styles.
-  // Unfortunately JSDOM doesn't handle css variables properly
-  // See: https://github.com/jsdom/jsdom/issues/1895
-  // @ts-ignore
-  const styles = element[Object.keys(element)[0]].memoizedProps.style;
-  let css = styleElement.textContent || '';
+  const stylesToFind = mapProperties(properties);
+  const foundStyles: string[] = [];
 
-  if (styles && Object.keys(styles).length > 0) {
-    Object.entries(styles).forEach(([key, value]: [string, any]) => {
-      // Replace all instances of var with the value.
-      // We split and join to replace all instances without needing to jump into a dynamic regex.
-      css = css.split(`var(${key})`).join(value);
-    });
+  for (const styleElement of styleElements) {
+    let css = styleElement.textContent || '';
+    // This is a hack to get ahold of the styles.
+    // Unfortunately JSDOM doesn't handle css variables properly
+    // See: https://github.com/jsdom/jsdom/issues/1895
+    // @ts-ignore
+    const styles = element[Object.keys(element)[0]].memoizedProps.style;
+
+    if (styles && Object.keys(styles).length > 0) {
+      Object.entries(styles).forEach(([key, value]: [string, any]) => {
+        // Replace all instances of var with the value.
+        // We split and join to replace all instances without needing to jump into a dynamic regex.
+        css = css.split(`var(${key})`).join(value);
+      });
+    }
+
+    foundStyles.push(...stylesToFind.filter(styleToFind => css.includes(styleToFind)));
   }
 
-  const stylesToFind = Object.keys(properties).map(
-    property => `${kebabCase(property)}:${properties[property]}`
-  );
-  const foundStyles = stylesToFind.filter(styleToFind => css.includes(styleToFind));
-  const notFoundStyles = stylesToFind.filter(styleToFind => !css.includes(styleToFind));
-  const includedSelector = css.includes(`.${element.className}`);
+  const notFoundStyles = stylesToFind.filter(style => !foundStyles.includes(style));
+  const foundFormatted = stylesToFind.join(', ');
+  const notFoundFormatted = notFoundStyles.join(', ');
 
-  if (includedSelector && foundStyles.length > 0 && notFoundStyles.length === 0) {
+  if (foundStyles.length > 0 && notFoundStyles.length === 0) {
     return {
       pass: true,
       message: !this.isNot
         ? () => ''
-        : () => `Found "${foundStyles.join(', ')}" on <${element.nodeName.toLowerCase()} ${styles &&
-            `style={${JSON.stringify(styles)}}`}> element.
-
-  Reconciled css (css variables replaced with actual values):
-  ${css}
-
-  Original css:
-  ${(styleElement && styleElement.textContent) || ''}
-  `,
+        : () =>
+            `Found "${foundFormatted}" on <${element.nodeName.toLowerCase()} css={\`${getMountedProperties()}\`}> element.`,
     };
   }
 
   return {
     pass: false,
-    message: () => `Could not find "${notFoundStyles.join(
-      ', '
-    )}" on <${element.nodeName.toLowerCase()} ${styles &&
-      `style={${JSON.stringify(styles)}}`}> element.
-
-Reconciled css (css variables replaced with actual values):
-${css}
-
-Original css:
-${(styleElement && styleElement.textContent) || ''}
-`,
+    message: () =>
+      `Could not find "${notFoundFormatted}" on <${element.nodeName.toLowerCase()} css={\`${getMountedProperties()}\`}> element.`,
   };
 }
