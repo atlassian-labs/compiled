9bca33beaa8c8178719a26af8e602a6a9ade39a9
madou <laheen@gmail.com>
2020-04-18 10:31:29 +1000

    chore: remove @import support to reduce bundle size

--- packages/style/src/sheet.tsx
+++ packages/style/src/sheet.tsx
@@ -1,3 +1,7 @@
+/**
+ * Mostly ripped out of Emotion https://github.com/emotion-js/emotion and then heavily modified to be even smaller.
+ * Thanks everyone who contributed in some form or another.
+ */
 import { StyleSheetOpts } from './types';
 
 // @ts-ignore - potentially returning undefined by ignorign to save kb.
@@ -44,28 +48,11 @@ export const createStyleSheet = (opts: StyleSheetOpts) => {
       const sheet = getStyleElementSheet(latestStyleElement);
 
       try {
-        // this is a really hot path
-        // we check the second character first because having "i"
-        // as the second character will happen less often than
-        // having "@" as the first character
-        const isImportRule = css.charCodeAt(1) === 105 && css.charCodeAt(0) === 64;
-        // this is the ultrafpast version, works across browsers
-        // the big drawback is that the css won't be editable in devtools
-        sheet.insertRule(
-          css,
-          // we need to insert @import rules before anything else
-          // otherwise there will be an error
-          // technically this means that the @import rules will
-          // _usually_(not always since there could be multiple style elements)
-          // be the first ones in prod and generally later in dev
-          // this shouldn't really matter in the real world though
-          // @import is generally only used for font faces from google fonts and etc.
-          // so while this could be technically correct then it would be slower and larger
-          // for a tiny bit of correctness that won't matter in the real world
-          isImportRule ? 0 : sheet.cssRules.length
-        );
+        // this is the ultrafast version, works across browsers
+        // the big drawback is that the css won't be editable in devtools in most browsers.
+        sheet.insertRule(css, sheet.cssRules.length);
       } catch (e) {
-        if (process.env.NODE_ENV !== 'production') {
+        if (process.env.NODE_ENV === 'development') {
           console.error(
             `
  ██████╗ ██████╗ ███╗   ███╗██████╗ ██╗██╗     ███████╗██████╗
