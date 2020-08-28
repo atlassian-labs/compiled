0263c4ebefc3334a183e386117b6c9434c7385bc
Prashant Goel <prashantmanunited@gmail.com>
2020-08-28 16:57:09 +0530

    docs: [#257] add storybook examples

---
+++ examples/styled-interpolation-function-behavior.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { styled } from '@compiled/core';
+
+export default {
+  title: 'styled components interpolation function behavior',
+};
+
+interface Props {
+  color: string;
+  bgColor: string;
+  textSize: number;
+  borderStyle: string;
+  padding: number;
+}
+
+const FunctionStyledObjectLiteral = styled.div<Props>({
+  color: (props) => props.color,
+  fontSize: (props) => `${props.textSize}px`,
+  backgroundColor: (props) => {
+    return props.bgColor;
+  },
+  border: `5px ${(props: Props) => props.borderStyle} black`,
+  padding: `${(props: Props) => {
+    return props.padding;
+  }}px`,
+});
+
+const FunctionStyledTemplateLiteral = styled.div<Props>`
+  color: ${(props) => props.color};
+  font-size: ${(props) => props.textSize}px;
+  background-color: ${(props) => {
+    return props.bgColor;
+  }};
+  border: 5px ${(props) => props.borderStyle} black;
+  padding: ${(props) => {
+    return props.padding;
+  }}px;
+`;
+
+export const ObjectLiteral = () => (
+  <FunctionStyledObjectLiteral
+    color="blue"
+    bgColor="red"
+    textSize={18}
+    borderStyle="dashed"
+    padding={8}>
+    hello world
+  </FunctionStyledObjectLiteral>
+);
+
+export const TemplateLiteral = () => (
+  <FunctionStyledTemplateLiteral
+    color="red"
+    bgColor="blue"
+    textSize={20}
+    borderStyle="dotted"
+    padding={10}>
+    hello world
+  </FunctionStyledTemplateLiteral>
+);
