0f83e1d597693a53406e7f3af69289d040172c56
Michael Dougall <mdougall@atlassian.com>
2020-09-10 09:14:56 +1000

    chore: adds size deps

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  plugins: [['@compiled/babel-plugin', { nonce: '"k0Mp1lEd"' }]],
+  plugins: [['@compiled/babel-plugin', { nonce: '"k0Mp1lEd"', minify: true }]],
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
