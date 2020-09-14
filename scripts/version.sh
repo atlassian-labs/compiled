0d892eea1dc18b2250d6794d0e81a36f4a3cd0f7
Michael Dougall <mdougall@atlassian.com>
2020-09-14 15:49:26 +1000

    chore: corrects workspaces run to only run over compiled pkgs

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,5 +6,5 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 
-npx workspaces-run -- npm version $PACKAGE_VERSION --allow-same-version
-npx workspaces-run -- node ../../scripts/set-compiled-deps.js --version $PACKAGE_VERSION
+npx workspaces-run --only="@compiled/*" -- npm version $PACKAGE_VERSION --allow-same-version
+npx workspaces-run --only="@compiled/*" -- node ../../scripts/set-compiled-deps.js --version $PACKAGE_VERSION
