dc0702070ee50a2613f4f1e26e047a2550c1f230
Michael Dougall <mdougall@atlassian.com>
2020-09-24 14:50:40 +1000

    feat: adds margin expand

--- packages/css/src/plugins/__tests__/property-expander.test.tsx
+++ packages/css/src/plugins/__tests__/property-expander.test.tsx
@@ -10,14 +10,62 @@ const transform = (css: TemplateStringsArray) => {
 };
 
 describe('property expander', () => {
-  it('should expand margin', () => {
+  it('should expand margin single', () => {
     const result = transform`
       margin: 0;
     `;
 
     expect(result).toMatchInlineSnapshot(`
       "
-            margin: 0;
+            margin-top: 0;
+            margin-right: 0;
+            margin-bottom: 0;
+            margin-left: 0;
+          "
+    `);
+  });
+
+  it('should expand margin double', () => {
+    const result = transform`
+      margin: 0 auto;
+    `;
+
+    expect(result).toMatchInlineSnapshot(`
+      "
+            margin-top: 0;
+            margin-right: auto;
+            margin-bottom: 0;
+            margin-left: auto;
+          "
+    `);
+  });
+
+  it('should expand margin triple', () => {
+    const result = transform`
+      margin: 1px 2px 3px;
+    `;
+
+    expect(result).toMatchInlineSnapshot(`
+      "
+            margin-top: 1px;
+            margin-right: 2px;
+            margin-bottom: 3px;
+            margin-left: 2px;
+          "
+    `);
+  });
+
+  it('should expand margin quadruple', () => {
+    const result = transform`
+      margin: 1px 2px 3px 4px;
+    `;
+
+    expect(result).toMatchInlineSnapshot(`
+      "
+            margin-top: 1px;
+            margin-right: 2px;
+            margin-bottom: 3px;
+            margin-left: 4px;
           "
     `);
   });
--- packages/css/src/plugins/property-expander.tsx
+++ packages/css/src/plugins/property-expander.tsx
@@ -1,8 +1,35 @@
-import { plugin } from 'postcss';
+import { plugin, Declaration } from 'postcss';
+import { parse, Root as ValuesRoot } from 'postcss-values-parser';
+
+type ConversionFunction = (node: Declaration, value: ValuesRoot) => Declaration[];
+
+const shorthands: Record<string, ConversionFunction> = {
+  margin: (node, value) => {
+    const [top, right = top, bottom = top, left = right] = value.nodes;
+
+    return [
+      node.clone({ prop: 'margin-top', value: top }),
+      node.clone({ prop: 'margin-right', value: right }),
+      node.clone({ prop: 'margin-bottom', value: bottom }),
+      node.clone({ prop: 'margin-left', value: left }),
+    ];
+  },
+};
 
 /**
  * PostCSS plugin that expands shortform properties to their longform equivalents.
  */
 export const propertyExpander = plugin('property-expander', () => {
-  return () => {};
+  return (root) => {
+    root.walkDecls(Object.keys(shorthands).join('|'), (decl) => {
+      const valueNode = parse(decl.value);
+      const convert = shorthands[decl.prop];
+
+      if (convert) {
+        const longforms = convert(decl, valueNode);
+        decl.parent.insertBefore(decl, longforms);
+        decl.remove();
+      }
+    });
+  };
 });
