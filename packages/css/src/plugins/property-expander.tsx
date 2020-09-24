5c13ac9f3c336b99270c0de71c62ddd8b38057c7
Michael Dougall <mdougall@atlassian.com>
2020-09-24 14:56:43 +1000

    feat: adds padding expand

--- packages/css/src/plugins/property-expander.tsx
+++ packages/css/src/plugins/property-expander.tsx
@@ -14,14 +14,26 @@ const shorthands: Record<string, ConversionFunction> = {
       node.clone({ prop: 'margin-left', value: left }),
     ];
   },
+  padding: (node, value) => {
+    const [top, right = top, bottom = top, left = right] = value.nodes;
+
+    return [
+      node.clone({ prop: 'padding-top', value: top }),
+      node.clone({ prop: 'padding-right', value: right }),
+      node.clone({ prop: 'padding-bottom', value: bottom }),
+      node.clone({ prop: 'padding-left', value: left }),
+    ];
+  },
 };
+
+const filter = new RegExp(Object.keys(shorthands).join('|'));
 
 /**
  * PostCSS plugin that expands shortform properties to their longform equivalents.
  */
 export const propertyExpander = plugin('property-expander', () => {
   return (root) => {
-    root.walkDecls(Object.keys(shorthands).join('|'), (decl) => {
+    root.walkDecls(filter, (decl) => {
       const valueNode = parse(decl.value);
       const convert = shorthands[decl.prop];
 
