f19b5df9be75cf15aee7a1cf306cb8d2abb083a7
DOUGES <madou@users.noreply.github.com>
2020-09-28 14:56:34 +1000

    Atomic post css plugin (#299)
    * feat: adds atomicify plugin for decls
    
    * feat: adds class name callback
    
    * feat: adds rule handling to atomicify plugin
    
    * feat: throw when nested rule
    
    * fix: ensure created hash is consistent with semantically same but different rules
    
    * fix: handles nesting operator in selector
    
    * chore: adds test stubs
    
    * feat: adds atrule handling to plugin
    
    * chore: adds jsdoc
    
    * feat: adds multi selector support to atomic plugin
    
    * chore: adds tests for nested autoprefixing at rules
    
    * fix: fixes at rules blowing up during autoprefixer
    
    * fix: ensure callback is passed to atrules
    
    * fix: ignore unhandled at rules
    
    * fix: handle comments, improve error messaging

---
+++ packages/css/src/plugins/__tests__/atomicify-rules.test.tsx
@@ -0,0 +1,376 @@
+import postcss from 'postcss';
+import whitespace from 'postcss-normalize-whitespace';
+import autoprefixer from 'autoprefixer';
+import nested from 'postcss-nested';
+import { atomicifyRules } from '../atomicify-rules';
+
+const transform = (css: TemplateStringsArray) => {
+  const result = postcss([atomicifyRules(), whitespace, autoprefixer]).process(css[0], {
+    from: undefined,
+  });
+
+  return result.css;
+};
+
+describe('atomicify rules', () => {
+  beforeEach(() => {
+    process.env.BROWSERSLIST = 'last 1 version';
+  });
+
+  it('should atomicify a single declaration', () => {
+    const actual = transform`
+      color: blue;
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._1doq13q2{color:blue}"`);
+  });
+
+  it('should should atomicify multiple declarations', () => {
+    const actual = transform`
+      color: blue;
+      font-size: 12px;
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._1doq13q2{color:blue}._36l61fwx{font-size:12px}"`);
+  });
+
+  it('should autoprefix atomic rules', () => {
+    process.env.BROWSERSLIST = 'Edge 16';
+
+    const result = transform`user-select: none;`;
+
+    expect(result).toMatchInlineSnapshot(`"._q4hxglyw{-ms-user-select:none;user-select:none}"`);
+  });
+
+  it('should autoprefix atomic rules with multiple selectors', () => {
+    process.env.BROWSERSLIST = 'Edge 16';
+
+    const result = transform`
+      :hover, :focus {
+        user-select: none;
+      }
+    `;
+
+    expect(result).toMatchInlineSnapshot(
+      `"._1tclglyw:hover, ._16wpglyw:focus{-ms-user-select:none;user-select:none}"`
+    );
+  });
+
+  it('should autoprefix atrule atomic rules', () => {
+    process.env.BROWSERSLIST = 'Edge 16';
+
+    const result = transform`
+      @media (min-width: 30rem) {
+        user-select: none;
+      }
+    `;
+
+    expect(result).toMatchInlineSnapshot(
+      `"@media (min-width: 30rem){._3r8kglyw{-ms-user-select:none;user-select:none}}"`
+    );
+  });
+
+  it('should autoprefix atrule nested atomic rules', () => {
+    process.env.BROWSERSLIST = 'Edge 16';
+
+    const result = transform`
+      @media (min-width: 30rem) {
+        div {
+          user-select: none;
+        }
+      }
+    `;
+
+    expect(result).toMatchInlineSnapshot(
+      `"@media (min-width: 30rem){._1a7jglyw div{-ms-user-select:none;user-select:none}}"`
+    );
+  });
+
+  it('should autoprefix nested atrule atomic rules', () => {
+    process.env.BROWSERSLIST = 'Edge 16';
+
+    const result = transform`
+      @media (min-width: 30rem) {
+        @media (min-width: 20rem) {
+          user-select: none;
+        }
+      }
+    `;
+
+    expect(result).toMatchInlineSnapshot(
+      `"@media (min-width: 30rem){@media (min-width: 20rem){._1cg4glyw{-ms-user-select:none;user-select:none}}}"`
+    );
+  });
+
+  it('should callback with created class names', () => {
+    const classes: string[] = [];
+    const callback = (className: string) => {
+      classes.push(className);
+    };
+
+    const result = postcss([atomicifyRules({ callback }), whitespace]).process(
+      `
+        display:block;
+        text-align:center;
+        @media (min-width: 30rem) {
+          @media (min-width: 20rem) {
+            user-select: none;
+          }
+        }
+        div, span, :hover {
+          user-select: none;
+        }
+      `,
+      {
+        from: undefined,
+      }
+    );
+
+    // Need to call this to fire the transformation.
+    result.css;
+
+    expect(classes).toMatchInlineSnapshot(`
+      Array [
+        "_dj7i1ule",
+        "_o3nk1h6o",
+        "_1cg4glyw",
+        "_1qcvglyw",
+        "_1uoyglyw",
+        "_1tclglyw",
+      ]
+    `);
+  });
+
+  it('should atomicify a nested tag with class rule', () => {
+    const actual = transform`
+      div.primary {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._yjs513q2 div.primary{color:blue}"`);
+  });
+
+  it('should atomicify a nested multi selector rule', () => {
+    const actual = transform`
+      div, span, li {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(
+      `"._k2hc13q2 div, ._ijgx13q2 span, ._1jah13q2 li{color:blue}"`
+    );
+  });
+
+  it('should atomicify a multi dangling pseudo rule', () => {
+    const actual = transform`
+      :hover, :focus {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._1uhh13q2:hover, ._t5gl13q2:focus{color:blue}"`);
+  });
+
+  it('should atomicify a nested tag rule', () => {
+    const actual = transform`
+      div {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._k2hc13q2 div{color:blue}"`);
+  });
+
+  it('should generate the same class hash for semantically same but different rules', () => {
+    const firstActual = transform`
+      &:first-child {
+        color: blue;
+      }
+    `;
+    const secondActual = transform`
+      :first-child {
+        color: blue;
+      }
+    `;
+
+    const expected = '._roi113q2:first-child{color:blue}';
+    expect(firstActual).toEqual(expected);
+    expect(secondActual).toEqual(expected);
+  });
+
+  it('should atomicify a rule when its selector has a nesting at the end', () => {
+    const actual = transform`
+      :first-child & {
+        color: hotpink;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._1qab1q9v:first-child ._1qab1q9v{color:hotpink}"`);
+  });
+
+  it('should reference the atomic class with the nesting selector', () => {
+    const actual = transform`
+      & :first-child {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._p9sj13q2 :first-child{color:blue}"`);
+  });
+
+  it('should atomicify a double tag rule', () => {
+    const actual = transform`
+      div span {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._m59i13q2 div span{color:blue}"`);
+  });
+
+  it('should atomicify a double tag with pseudos rule', () => {
+    const actual = transform`
+      div:hover span:active {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._107g13q2 div:hover span:active{color:blue}"`);
+  });
+
+  it('should atomicify a nested tag pseudo rule', () => {
+    const actual = transform`
+      div:hover {
+        color: blue;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(`"._5tvz13q2 div:hover{color:blue}"`);
+  });
+
+  it('should skip comments', () => {
+    const actual = transform`
+      /* hello world */
+      div:hover {
+        /* hello world */
+        color: blue;
+      }
+
+      @media screen {
+        /* hello world */
+        color: red;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(
+      `"._5tvz13q2 div:hover{color:blue}@media screen{._gli45scu{color:red}}"`
+    );
+  });
+
+  it('should blow up if a doubly nested rule was found', () => {
+    expect(() => {
+      transform`
+        div {
+          div {
+            font-size: 12px;
+          }
+        }
+      `;
+    }).toThrow(
+      'atomicify-rules: <css input>:3:11: Nested rules need to be flattened first - run the "postcss-nested" plugin before this.'
+    );
+  });
+
+  it('should not blow up if a doubly nested rule was found after nested plugin', () => {
+    const result = postcss([nested, atomicifyRules(), whitespace, autoprefixer]).process(
+      `
+      div {
+        div {
+          font-size: 12px;
+        }
+      }
+    `,
+      {
+        from: undefined,
+      }
+    );
+
+    expect(result.css).toMatchInlineSnapshot(`"._1qan1fwx div div{font-size:12px}"`);
+  });
+
+  it('should atomicify at rule styles', () => {
+    const actual = transform`
+      @media (min-width: 30rem) {
+        display: block;
+        font-size: 20px;
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(
+      `"@media (min-width: 30rem){._1ie31ule{display:block}._4ubngktf{font-size:20px}}"`
+    );
+  });
+
+  it('should atomicify nested at rule styles', () => {
+    const actual = transform`
+      @media (min-width: 30rem) {
+        @media (min-width: 20rem) {
+          display: block;
+        }
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(
+      `"@media (min-width: 30rem){@media (min-width: 20rem){._16pr1ule{display:block}}}"`
+    );
+  });
+
+  it('should atomicify at rule nested styles', () => {
+    const actual = transform`
+      @media (min-width: 30rem) {
+        div {
+          display: block;
+        }
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(
+      `"@media (min-width: 30rem){._166e1ule div{display:block}}"`
+    );
+  });
+
+  it('should atomicify double nested at rule nested styles', () => {
+    const actual = transform`
+      @media (min-width: 30rem) {
+        @media (min-width: 20rem) {
+          div {
+            display: block;
+          }
+        }
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(
+      `"@media (min-width: 30rem){@media (min-width: 20rem){._15ac1ule div{display:block}}}"`
+    );
+  });
+
+  it('should ignore unhanded at rules', () => {
+    const actual = transform`
+      @charset 'utf-8';
+      @import 'custom.css';
+      @namespace 'XML-namespace-URL';
+
+      @keyframes hello-world { from: { opacity: 0 } to { opacity: 1 } }
+      @font-face {
+        font-family: "Open Sans";
+      }
+    `;
+
+    expect(actual).toMatchInlineSnapshot(
+      `"@charset 'utf-8';@import 'custom.css';@namespace 'XML-namespace-URL';@-webkit-keyframes hello-world{from:{opacity:0}to{opacity:1}}@keyframes hello-world{from:{opacity:0}to{opacity:1}}@font-face{font-family:\\"Open Sans\\"}"`
+    );
+  });
+});
---
+++ packages/css/src/plugins/atomicify-rules.tsx
@@ -0,0 +1,231 @@
+import { plugin, Node, Declaration, decl, Container, rule, Rule, AtRule } from 'postcss';
+import { hash } from '@compiled/utils';
+
+interface PluginOpts {
+  callback?: (className: string) => void;
+}
+
+interface AtomicifyOpts extends PluginOpts {
+  selectors?: string[];
+  atRule?: string;
+  parentNode?: Container;
+}
+
+/**
+ * Returns an atomic rule class name using this form:
+ *
+ * ```
+ * "_{atrules}{selectors}{propertyname}"
+ * ```
+ *
+ * Atomic rules are always prepended with an underscore.
+ *
+ * @param group
+ * @param value
+ */
+const atomicClassName = (propName: string, value: string, opts: AtomicifyOpts) => {
+  const selectors = opts.selectors ? opts.selectors.join('') : '';
+  const group = hash(`${opts.atRule}${selectors}${propName}`).slice(0, 4);
+  const valueHash = hash(value).slice(0, 4);
+
+  return `_${group}${valueHash}`;
+};
+
+/**
+ * Returns a normalized selector.
+ * This will handle immediate dangling pseudos `:` as well as the nesting operator `&`.
+ *
+ * @param selector
+ */
+const normalizeSelector = (selector: string | undefined) => {
+  if (!selector) {
+    // Nothing to see here - return early with an empty string!
+    return '';
+  }
+
+  // We want to build a consistent selector that we will use to generate the group hash.
+  // Because of that we trim whitespace as well as removing any top level nesting "&".
+  const trimmed = selector.trim();
+
+  switch (trimmed.charAt(0)) {
+    case ':':
+      // Dangling pseudo - return immedately!
+      return trimmed;
+
+    case '&':
+      // Dangling nesting - replace it and return!
+      return trimmed.replace('&', '');
+
+    default:
+      // Must be a nested selector - add a space before it!
+      return ` ${trimmed}`;
+  }
+};
+
+/**
+ * Replaces all instances of a nesting operator `&` with the parent class name.
+ *
+ * @param selector
+ * @param parentClassName
+ */
+const replaceNestingSelector = (selector: string, parentClassName: string) => {
+  return selector.replace(/ &/g, ` .${parentClassName}`);
+};
+
+/**
+ * Builds an atomic rule selector.
+ *
+ * @param node
+ */
+const buildAtomicSelector = (node: Declaration, opts: AtomicifyOpts) => {
+  const selectors: string[] = [];
+
+  (opts.selectors || ['']).forEach((selector) => {
+    const normalizedSelector = normalizeSelector(selector);
+    const className = atomicClassName(node.prop, node.value, {
+      ...opts,
+      selectors: [normalizedSelector],
+    });
+    const replacedSelector = replaceNestingSelector(normalizedSelector, className);
+
+    selectors.push(`.${className}${replacedSelector}`);
+
+    if (opts.callback) {
+      opts.callback(className);
+    }
+  });
+
+  return selectors.join(', ');
+};
+
+/**
+ * Transforms a declaration into an atomic rule.
+ *
+ * @param node
+ * @param opts
+ */
+const atomicifyDecl = (node: Declaration, opts: AtomicifyOpts) => {
+  const selector = buildAtomicSelector(node, opts);
+  const newDecl = decl({ prop: node.prop, value: node.value });
+  const newRule = rule({ selector, nodes: [newDecl] });
+
+  // We need to link the new node to a parent else autoprefixer blows up.
+  newDecl.parent = newRule;
+  newDecl.raws.before = '';
+  newRule.parent = opts.parentNode!;
+  newRule.raws.before = '';
+
+  return newRule;
+};
+
+/**
+ * Transforms a rule into atomic rules.
+ *
+ * @param node
+ * @param opts
+ */
+const atomicifyRule = (node: Rule, opts: AtomicifyOpts): Rule[] => {
+  if (!node.nodes) {
+    return [];
+  }
+
+  return node.nodes
+    .map((childNode) => {
+      if (childNode.type === 'rule') {
+        throw childNode.error(
+          'Nested rules need to be flattened first - run the "postcss-nested" plugin before this.'
+        );
+      }
+
+      if (childNode.type !== 'decl') {
+        return undefined;
+      }
+
+      return atomicifyDecl(childNode, {
+        ...opts,
+        selectors: node.selectors,
+      });
+    })
+    .filter((child): child is Rule => !!child);
+};
+
+/**
+ * Transforms an atrule into atomic rules.
+ *
+ * @param node
+ * @param opts
+ */
+const atomicifyAtRule = (node: AtRule, opts: AtomicifyOpts): AtRule => {
+  const children: Node[] = [];
+  const newNode = node.clone({ nodes: children });
+  const atRuleLabel = `${opts.atRule || ''}${node.name}${node.params}`;
+  const atRuleOpts = {
+    ...opts,
+    parentNode: newNode,
+    atRule: atRuleLabel,
+  };
+
+  newNode.parent = opts.parentNode!;
+
+  node.each((childNode) => {
+    switch (childNode.type) {
+      case 'atrule':
+        children.push(atomicifyAtRule(childNode, atRuleOpts));
+        break;
+
+      case 'rule':
+        atomicifyRule(childNode, atRuleOpts).forEach((rule) => {
+          children.push(rule);
+        });
+        break;
+
+      case 'decl':
+        children.push(atomicifyDecl(childNode, atRuleOpts));
+        break;
+
+      default:
+        break;
+    }
+  });
+
+  return newNode;
+};
+
+/**
+ * Transforms a style sheet into atomic rules.
+ * When passing a `callback` option it will callback with created class names.
+ *
+ * Preconditions:
+ *
+ * 1. No nested rules allowed - normalize them with the `parent-orphaned-pseudos` and `nested` plugins first.
+ */
+export const atomicifyRules = plugin<PluginOpts>('atomicify-rules', (opts = {}) => {
+  return (root) => {
+    root.each((node) => {
+      switch (node.type) {
+        case 'atrule':
+          const supported = ['media', 'supports', 'document'];
+          if (supported.includes(node.name)) {
+            node.replaceWith(atomicifyAtRule(node, opts));
+          }
+
+          break;
+
+        case 'rule':
+          node.replaceWith(atomicifyRule(node, opts));
+          break;
+
+        case 'decl':
+          node.replaceWith(atomicifyDecl(node, opts));
+          break;
+
+        case 'comment':
+          node.remove();
+          break;
+
+        default:
+          break;
+      }
+    });
+  };
+});
