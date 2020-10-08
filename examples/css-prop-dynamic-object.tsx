076ab5da2c17faaae1ead1c24e2e03fdd850e72f
Michael Dougall <mdougall@atlassian.com>
2020-10-08 17:37:31 +1100

    fix: fixes duplicate declarations not being removed on css prop

--- examples/css-prop-dynamic-object.tsx
+++ examples/css-prop-dynamic-object.tsx
@@ -16,7 +16,7 @@ export const DynamicCssProp = () => {
           backgroundColor: 'purple',
         },
       }}>
-      <div css={{ display: 'flex', fontSize: '20px', color }}>Hello, world!</div>
+      <div css={{ display: 'flex', fontSize: '20px', color }}>{color} text</div>
 
       <div
         css={{
@@ -34,7 +34,7 @@ export const DynamicCssProp = () => {
           display: block;
           font-size: 20px;
         `}>
-        strip yeah
+        black text
       </div>
     </div>
   );
