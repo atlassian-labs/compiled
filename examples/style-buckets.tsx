d91a7931a474bd389327ad7dee63d26d68b9f771
Prashant Goel <prashantmanunited@gmail.com>
2020-10-20 10:26:48 +0530

    chore: add buckets storybook example

---
+++ examples/style-buckets.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import '@compiled/core';
+
+import { hover } from './mixins/mixins';
+
+export default {
+  title: 'style buckets',
+};
+
+export const Example = () => {
+  return (
+    <a
+      href="https://atlassian.design"
+      css={[
+        {
+          display: 'flex',
+          fontSize: '50px',
+          color: 'purple',
+          ':hover': hover,
+          ':active': {
+            color: 'blue',
+          },
+          ':link': {
+            color: 'red',
+          },
+          ':focus': {
+            color: 'green',
+          },
+          ':visited': {
+            color: 'pink',
+          },
+        },
+        `
+        @media (max-width: 800px) {
+          :active { color: black; }
+          :focus { color: yellow; }
+        }
+      `,
+      ]}>
+      Atlassian Design System
+    </a>
+  );
+};
