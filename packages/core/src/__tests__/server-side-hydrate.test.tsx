308cae5888be5001d28aa215404404810aba6a2d
Prashant Goel <prashantmanunited@gmail.com>
2020-11-03 09:52:45 +0530

    chore: fix few nits

--- packages/core/src/__tests__/server-side-hydrate.test.tsx
+++ packages/core/src/__tests__/server-side-hydrate.test.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { styled } from '@compiled/core';
 
 describe('server side hydrate', () => {
-  it('should ssr then hydrate element correctly', () => {
+  it('it should not log unexpected warnings when hydrating SSRing', () => {
     jest.spyOn(global.console, 'error').mockImplementation(() => {});
 
     const StyledDiv = styled.div`
@@ -27,6 +27,9 @@ describe('server side hydrate', () => {
     // Restore the release name after hydration is complete
     process.release.name = releaseName;
 
+    // We will get mismatches for the style rendering but this is expected because
+    // we inline the style elements in the server rendered markup and then on the client
+    // when the JavaScript initializes they get moved to the head of the document.
     const mockCalls = (console.error as jest.Mock).mock.calls.filter(
       ([f, s]) =>
         !(f === 'Warning: Did not expect server HTML to contain a <%s> in <%s>.%s' && s === 'style')
