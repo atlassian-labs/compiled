53d17f48c14783fbe97cb97a2c2cd6b1aeb793b9
Michael Dougall <mdougall@atlassian.com>
2020-11-13 19:44:13 +1100

    chore: adds ax benchmark

---
+++ benchmarks/ax-benchmark.tsx
@@ -0,0 +1,41 @@
+import { ax } from '@compiled/core';
+import Benchmark, { Event as BenchmarkEvent } from 'benchmark';
+
+console.log('Start ax benchmarking');
+console.log();
+
+const suite = new Benchmark.Suite('ax');
+
+suite
+  .add('array', () => {
+    ax([
+      '_19itglyw',
+      '_2rko1l7b',
+      '_ca0qftgi',
+      '_u5f319bv',
+      '_n3tdftgi',
+      '_19bv19bv',
+      '_bfhk1mzw',
+      '_syazu67f',
+      '_k48p1nn1',
+      '_ect41kw7',
+      '_1wybdlk8',
+      '_irr3mlcl',
+      '_1di6vctu',
+      undefined,
+    ]);
+  })
+  .add('single string', () => {
+    ax([
+      '_19itglyw _2rko1l7b _ca0qftgi _u5f319bv _n3tdftgi _19bv19bv _bfhk1mzw _syazu67f _k48p1nn1 _ect41kw7 _1wybdlk8 _irr3mlcl _1di6vctu',
+      undefined,
+    ]);
+  })
+  .on('cycle', (event: BenchmarkEvent) => {
+    console.log('==> ', String(event.target));
+  })
+  .on('complete', () => {
+    console.log();
+    console.log(`Fastest is ${suite.filter('fastest').map(({ name }: { name: string }) => name)}`);
+  })
+  .run({ async: true });
