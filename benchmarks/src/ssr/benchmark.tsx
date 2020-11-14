700b5d8259dacea15ed818adce63cdaf20588c14
Michael Dougall <mdougall@atlassian.com>
2020-11-14 12:11:51 +1100

    chore: adds two styles to playground

--- benchmarks/src/ssr/benchmark.tsx
+++ benchmarks/src/ssr/benchmark.tsx
@@ -2,37 +2,10 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import Benchmark, { Event as BenchmarkEvent } from 'benchmark';
 import { CC, CS } from '@compiled/core';
+import { CompiledComponent, StyleArr, StyleStr } from './playground';
 
 console.log('Start ssr benchmarking');
 console.log();
-
-const App = () => (
-  <CC>
-    <CS>
-      {[
-        '._s7n4jp4b{vertical-align:top}',
-        '._1reo15vq{overflow-x:hidden}',
-        '._18m915vq{overflow-y:hidden}',
-        '._1bto1l2s{text-overflow:ellipsis}',
-        '._o5721q9c{white-space:nowrap}',
-        '._ca0qidpf{padding-top:0}',
-        '._u5f31y44{padding-right:4px}',
-        '._n3tdidpf{padding-bottom:0}',
-        '._19bv1y44{padding-left:4px}',
-        '._p12f12xx{max-width:100px}',
-        '._1bsb1osq{width:100%}',
-      ]}
-    </CS>
-    <span
-      className="_bfhk1jys _2rko1l7b _vchhusvi _syaz4rde _1e0c1o8l _1wyb1skh _k48p1fw0 _vwz4kb7n _p12f1osq _ca0qyh40 _u5f3idpf _n3td1l7b _19bvidpf _1p1dangw _s7n41q9y"
-      // @ts-ignore
-      style={{ '--_16owtcm': 'rgb(227, 252, 239)', '--_kmurgp': 'rgb(0, 102, 68)' }}>
-      <span>Lozenge</span>
-    </span>
-  </CC>
-);
-
-renderToString(<App />);
 
 const suite = new Benchmark.Suite('ssr');
 
@@ -62,6 +35,50 @@ suite
           <span>Lozenge</span>
         </span>
       </CC>
+    );
+  })
+  .add('ssr no order arr', () => {
+    renderToString(
+      <CompiledComponent>
+        <StyleArr>
+          {[
+            '._s7n4jp4b{vertical-align:top}',
+            '._1reo15vq{overflow-x:hidden}',
+            '._18m915vq{overflow-y:hidden}',
+            '._1bto1l2s{text-overflow:ellipsis}',
+            '._o5721q9c{white-space:nowrap}',
+            '._ca0qidpf{padding-top:0}',
+            '._u5f31y44{padding-right:4px}',
+            '._n3tdidpf{padding-bottom:0}',
+            '._19bv1y44{padding-left:4px}',
+            '._p12f12xx{max-width:100px}',
+            '._1bsb1osq{width:100%}',
+          ]}
+        </StyleArr>
+        <span
+          className="_bfhk1jys _2rko1l7b _vchhusvi _syaz4rde _1e0c1o8l _1wyb1skh _k48p1fw0 _vwz4kb7n _p12f1osq _ca0qyh40 _u5f3idpf _n3td1l7b _19bvidpf _1p1dangw _s7n41q9y"
+          // @ts-ignore
+          style={{ '--_16owtcm': 'rgb(227, 252, 239)', '--_kmurgp': 'rgb(0, 102, 68)' }}>
+          <span>Lozenge</span>
+        </span>
+      </CompiledComponent>
+    );
+  })
+  .add('ssr no order string', () => {
+    renderToString(
+      <CompiledComponent>
+        <StyleStr>
+          {
+            '._s7n4jp4b{vertical-align:top}._1reo15vq{overflow-x:hidden}._18m915vq{overflow-y:hidden}._1bto1l2s{text-overflow:ellipsis}._o5721q9c{white-space:nowrap}._ca0qidpf{padding-top:0}._u5f31y44{padding-right:4px}._n3tdidpf{padding-bottom:0}._19bv1y44{padding-left:4px}._p12f12xx{max-width:100px}._1bsb1osq{width:100%}'
+          }
+        </StyleStr>
+        <span
+          className="_bfhk1jys _2rko1l7b _vchhusvi _syaz4rde _1e0c1o8l _1wyb1skh _k48p1fw0 _vwz4kb7n _p12f1osq _ca0qyh40 _u5f3idpf _n3td1l7b _19bvidpf _1p1dangw _s7n41q9y"
+          // @ts-ignore
+          style={{ '--_16owtcm': 'rgb(227, 252, 239)', '--_kmurgp': 'rgb(0, 102, 68)' }}>
+          <span>Lozenge</span>
+        </span>
+      </CompiledComponent>
     );
   })
   .on('cycle', (event: BenchmarkEvent) => {
---
+++ benchmarks/src/ssr/playground.tsx
@@ -0,0 +1,55 @@
+import React, { useContext, createContext } from 'react';
+
+const Cache = createContext<Record<string, true> | null>(null);
+
+export const useCache = () => {
+  return useContext(Cache) || {};
+};
+
+export const CompiledComponent = (props: { children: JSX.Element[] | JSX.Element }) => {
+  return <Cache.Provider value={useCache()}>{props.children}</Cache.Provider>;
+};
+
+export function StyleStr(props: any) {
+  const inserted = useCache();
+
+  // The following code will not exist in the browser bundle.
+  const sheets = props.children.split('.');
+  let toInsert = '';
+
+  for (let i = 0; i < sheets.length; i++) {
+    const sheet = sheets[i];
+    if (inserted[sheet]) {
+      continue;
+    }
+    toInsert += sheet;
+  }
+
+  if (!toInsert) {
+    return null;
+  }
+
+  return <style nonce={props.nonce}>{toInsert}</style>;
+}
+
+export function StyleArr(props: any) {
+  const inserted = useCache();
+
+  // The following code will not exist in the browser bundle.
+  const sheets = props.children;
+  let toInsert = '';
+
+  for (let i = 0; i < sheets.length; i++) {
+    const sheet = sheets[i];
+    if (inserted[sheet]) {
+      continue;
+    }
+    toInsert += sheet;
+  }
+
+  if (!toInsert) {
+    return null;
+  }
+
+  return <style nonce={props.nonce}>{toInsert}</style>;
+}
