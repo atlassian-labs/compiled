b5500a76ade9ad003026e4405b8b1402af1ca1de
Prashant Goel <prashantmanunited@gmail.com>
2020-11-19 10:39:01 +0530

    chore: revert back compiled and emotion perf tests

--- examples/compiled.tsx
+++ examples/compiled.tsx
@@ -1,46 +1,41 @@
 import * as React from 'react';
 import '@compiled/react';
-import { InteractionTaskArgs, PublicInteractionTask } from 'storybook-addon-performance';
-import { fireEvent, findAllByText } from '@testing-library/dom';
 
 export default {
   title: 'benchmarks | compiled',
 };
 
-const APPEARANCES = [
-  { bg: 'red', color: 'white' },
-  { bg: 'blue', color: 'white' },
-  { bg: 'green', color: 'black' },
-  { bg: 'grey', color: 'white' },
-  { bg: 'yellow', color: 'black' },
-  { bg: 'orange', color: 'white' },
-];
-
-const interactionTasks: PublicInteractionTask[] = [
-  {
-    name: 'Display lozenge',
-    run: async ({ container }: InteractionTaskArgs): Promise<void> => {
-      window.location.reload();
-
-      const buttonAll = container.querySelectorAll('button')!;
-
-      buttonAll.forEach((button) => {
-        fireEvent.click(button);
-      });
-
-      await findAllByText(container, 'Lozenge', undefined, {
-        timeout: 2000,
-      });
-    },
-  },
-];
-
-const Row: React.FunctionComponent<React.ReactNode> = ({ children }) => (
-  <div style={{ display: 'flex' }}>{children}</div>
-);
-
-const Col: React.FunctionComponent<React.ReactNode> = ({ children }) => (
-  <div style={{ flex: '1 1 auto' }}>{children}</div>
+export const Static = () => (
+  <span
+    css={{
+      backgroundColor: 'rgb(66, 82, 110)',
+      borderRadius: 3,
+      boxSizing: 'border-box',
+      color: 'rgb(255, 255, 255)',
+      display: 'inline-block',
+      fontSize: '11px',
+      fontWeight: 700,
+      lineHeight: 1,
+      maxWidth: '100%',
+      padding: '2px 0 3px 0',
+      textTransform: 'uppercase',
+      verticalAlign: 'baseline',
+    }}>
+    <span
+      css={{
+        display: 'inline-block',
+        verticalAlign: 'top',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        padding: `0 4px`,
+        maxWidth: 100,
+        width: '100%',
+      }}>
+      Lozenge
+    </span>
+  </span>
 );
 
 const Lozenge = (props: { bg: string; color: string }) => (
@@ -76,35 +71,4 @@ const Lozenge = (props: { bg: string; color: string }) => (
   </span>
 );
 
-export const Dynamic = () => {
-  const [toggle, setToggle] = React.useState<boolean[]>([]);
-
-  return Array.from(new Array(100)).map((_, index) => (
-    <React.Fragment key={index}>
-      <button onClick={() => setToggle([...toggle, !toggle[index]])}>Toggle</button>
-      {toggle[index] && (
-        <div>
-          <Row>
-            <Col>
-              <p>Colors</p>
-              {APPEARANCES.map((a) => (
-                <p key={a.bg as string}>
-                  <Lozenge bg={a.bg} color={a.color} />
-                </p>
-              ))}
-            </Col>
-          </Row>
-        </div>
-      )}
-    </React.Fragment>
-  ));
-};
-
-Dynamic.story = {
-  name: 'Compiled Dynamic',
-  parameters: {
-    performance: {
-      interactions: interactionTasks,
-    },
-  },
-};
+export const Dynamic = () => <Lozenge bg="rgb(227, 252, 239)" color="rgb(0, 102, 68)" />;
--- examples/emotion.tsx
+++ examples/emotion.tsx
@@ -1,47 +1,41 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import * as React from 'react';
-import { InteractionTaskArgs, PublicInteractionTask } from 'storybook-addon-performance';
-import { fireEvent, findAllByText } from '@testing-library/dom';
 
 export default {
   title: 'benchmarks | emotion',
 };
 
-const APPEARANCES = [
-  { bg: 'red', color: 'white' },
-  { bg: 'blue', color: 'white' },
-  { bg: 'green', color: 'black' },
-  { bg: 'grey', color: 'white' },
-  { bg: 'yellow', color: 'black' },
-  { bg: 'orange', color: 'white' },
-];
-
-const interactionTasks: PublicInteractionTask[] = [
-  {
-    name: 'Display lozenge',
-    run: async ({ container }: InteractionTaskArgs): Promise<void> => {
-      window.location.reload();
-
-      const buttonAll = container.querySelectorAll('button')!;
-
-      buttonAll.forEach((button) => {
-        fireEvent.click(button);
-      });
-
-      await findAllByText(container, 'Lozenge', undefined, {
-        timeout: 2000,
-      });
-    },
-  },
-];
-
-const Row: React.FunctionComponent<React.ReactNode> = ({ children }) => (
-  <div style={{ display: 'flex' }}>{children}</div>
-);
-
-const Col: React.FunctionComponent<React.ReactNode> = ({ children }) => (
-  <div style={{ flex: '1 1 auto' }}>{children}</div>
+export const Static = () => (
+  <span
+    css={{
+      backgroundColor: 'rgb(223, 225, 230)',
+      borderRadius: 3,
+      boxSizing: 'border-box',
+      color: 'rgb(66, 82, 110)',
+      display: 'inline-block',
+      fontSize: '11px',
+      fontWeight: 700,
+      lineHeight: 1,
+      maxWidth: '100%',
+      padding: '2px 0 3px 0',
+      textTransform: 'uppercase',
+      verticalAlign: 'baseline',
+    }}>
+    <span
+      css={{
+        display: 'inline-block',
+        verticalAlign: 'top',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        padding: `0 4px`,
+        maxWidth: 100,
+        width: '100%',
+      }}>
+      Lozenge
+    </span>
+  </span>
 );
 
 const Lozenge = (props: { bg: string; color: string }) => (
@@ -77,35 +71,4 @@ const Lozenge = (props: { bg: string; color: string }) => (
   </span>
 );
 
-export const Dynamic = () => {
-  const [toggle, setToggle] = React.useState<boolean[]>([]);
-
-  return Array.from(new Array(100)).map((_, index) => (
-    <React.Fragment key={index}>
-      <button onClick={() => setToggle([...toggle, !toggle[index]])}>Toggle</button>
-      {toggle[index] && (
-        <div>
-          <Row>
-            <Col>
-              <p>Colors</p>
-              {APPEARANCES.map((a) => (
-                <p key={a.bg as string}>
-                  <Lozenge bg={a.bg} color={a.color} />
-                </p>
-              ))}
-            </Col>
-          </Row>
-        </div>
-      )}
-    </React.Fragment>
-  ));
-};
-
-Dynamic.story = {
-  name: 'Emotion Dynamic',
-  parameters: {
-    performance: {
-      interactions: interactionTasks,
-    },
-  },
-};
+export const Dynamic = () => <Lozenge bg="rgb(0, 135, 90)" color="rgb(255, 255, 255)" />;
