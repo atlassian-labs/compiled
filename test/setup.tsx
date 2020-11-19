b9579eb2f383e96c059dfb314975811ab50a5a85
DOUGES <madou@users.noreply.github.com>
2020-11-19 15:54:10 +1100

    feat: adds comment to transformed files (#372)
    * feat: adds comment to transformed files
    
    * fix: parse filename only
    
    * fix: stablize test version

--- test/setup.tsx
+++ test/setup.tsx
@@ -3,3 +3,6 @@ import { toHaveCompiledCss } from '@compiled/jest';
 expect.extend({
   toHaveCompiledCss,
 });
+
+// Used to override pkg version in tests so it is stable across version changes.
+process.env.TEST_PKG_VERSION = '0.0.0';
