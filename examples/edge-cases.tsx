6fa3fd755db10caa7347c135aa4840ea08847a8f
Michael Dougall <mdougall@atlassian.com>
2020-12-22 14:25:58 +1100

    chore: adds content example

--- examples/edge-cases.tsx
+++ examples/edge-cases.tsx
@@ -18,3 +18,26 @@ export const PreventCSSVariablesLeaking = () => {
     </Container>
   );
 };
+
+const DynamicContent = styled.div<{ pre: string; post: string }>`
+  padding: 8px;
+  color: red;
+
+  :before {
+    content: '${(props) => props.pre}';
+    margin-right: 8px;
+    color: pink;
+  }
+
+  :after {
+    content: '${(props) => props.post}';
+    margin-left: 8px;
+    color: blue;
+  }
+`;
+
+export const ContentInterpolation = () => (
+  <DynamicContent pre="i am before" post="i am after">
+    middle
+  </DynamicContent>
+);
