f0c26dae4d7a08631b56667c74a46e0f6a5de164
Michael Dougall <mdougall@atlassian.com>
2021-01-12 16:31:16 +1100

    fix: removes hacks

--- packages/react/src/codemods/constants.tsx
+++ packages/react/src/codemods/constants.tsx
@@ -1,21 +1,3 @@
-/**
- * Because we don't want to include package json in the build artefacts
- * AND because the location of package json will be different depending
- * on using source or built assets - we do this. Not great but for now
- * it's better than re-writing the build setup.
- */
-const tryGetPkgJson = () => {
-  try {
-    return require('../../package.json');
-  } catch {
-    return require('../../../package.json');
-  }
-};
-
-const pkgJson = tryGetPkgJson();
-
-export const COMPILED_IMPORT_PATH = pkgJson.name;
-
+export const COMPILED_IMPORT_PATH = '@compiled/react';
 export const REACT_IMPORT_PATH = 'react';
-
 export const REACT_IMPORT_NAME = 'React';
