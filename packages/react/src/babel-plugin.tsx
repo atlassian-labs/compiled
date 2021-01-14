e60a94666b230eface39489cda71e05c712ef4ac
Michael Dougall <mdougall@atlassian.com>
2021-01-14 12:18:47 +1100

    chore: removes babel plugin from react pkg

--- packages/react/src/babel-plugin.tsx
+++
@@ -1,1 +0,0 @@
-module.exports = require('@compiled/babel-plugin');
--- test/test-imports.js
+++ test/test-imports.js
@@ -1,3 +1,3 @@
 require('@compiled/react');
 require('@compiled/react/runtime');
-require('@compiled/react/babel-plugin');
+require('@compiled/babel-plugin');
