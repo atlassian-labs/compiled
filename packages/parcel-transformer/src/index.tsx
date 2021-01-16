695c94e0146eaac03d485cd79114a8f679b404a8
Michael Dougall <mdougall@atlassian.com>
2021-01-16 13:14:17 +1100

    chore: configure parcelrc

--- packages/parcel-transformer/src/index.tsx
+++ packages/parcel-transformer/src/index.tsx
@@ -1,13 +1,10 @@
 import { Transformer } from '@parcel/plugin';
 import semver from 'semver';
 
-console.log('hmmm');
-
 export default new Transformer({
   async loadConfig(_: any) {},
 
   canReuseAST({ ast }: any) {
-    console.log(ast);
     return ast.type === 'babel' && semver.satisfies(ast.version, '^7.0.0');
   },
 
