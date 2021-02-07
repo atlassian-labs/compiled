390516a018ad5d5de7b70bfa6608c6adc276c3da
Michael Dougall <mdougall@atlassian.com>
2021-02-08 09:11:50 +1100

    chore: use add dep

--- packages/webpack-loader/src/index.tsx
+++ packages/webpack-loader/src/index.tsx
@@ -1,3 +1,11 @@
-export default function compiledLoader(content: string): string {
+import path from 'path';
+
+export default function compiledLoader(this: any, content: string): string {
+  console.log('BUNDLING', content);
+
+  this.addDependency(
+    path.normalize('/Users/mdougall/projects/compiled/examples/packages/webpack/src/app.js')
+  );
+
   return content;
 }
