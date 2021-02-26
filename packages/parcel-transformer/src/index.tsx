31b5f1d3ef5bc3c63706af8af83b90ba9c41d906
madou <laheen@gmail.com>
2021-02-27 09:12:37 +1100

    chore: pushing the rock up the hil

--- packages/parcel-transformer/src/index.tsx
+++ packages/parcel-transformer/src/index.tsx
@@ -2,7 +2,7 @@ import { Transformer } from '@parcel/plugin';
 import semver from 'semver';
 import compiledBabelPlugin from '@compiled/babel-plugin';
 import { parseAsync, transformFromAstAsync } from '@babel/core';
-import { generate } from '@parcel/babel-ast-utils';
+import generate from '@babel/generator';
 
 /**
  * Compiled parcel transformer.
@@ -14,6 +14,9 @@ export default new Transformer({
 
   async parse({ asset }: any) {
     const code = await asset.getCode();
+    if (code.indexOf('@compiled/react') === -1) {
+      return undefined;
+    }
 
     const ast = await parseAsync(code, {
       filename: asset.filePath,
@@ -25,44 +28,46 @@ export default new Transformer({
   },
 
   async transform({ asset, ast }: any) {
-    if (asset.isSource) {
-      const includedFiles: string[] = [];
+    if (!asset.isSource || !ast) {
+      // We're not interested in this asset.
+      return [asset];
+    }
 
-      const result = await transformFromAstAsync(
-        ast.program,
-        asset.isASTDirty() ? undefined : await asset.getCode(),
-        {
-          code: false,
-          ast: true,
-          filename: asset.filePath,
-          babelrc: false,
-          configFile: false,
-          plugins: [
-            [compiledBabelPlugin, { onIncludedFile: (file: string) => includedFiles.push(file) }],
-          ],
-          caller: {
-            name: 'compiled',
-          },
-        }
-      );
+    const includedFiles: string[] = [];
+    const code = asset.isASTDirty() ? undefined : await asset.getCode();
 
-      includedFiles.forEach((file) => {
-        asset.addIncludedFile(file);
+    const result = await transformFromAstAsync(ast.program, code, {
+      code: false,
+      ast: true,
+      filename: asset.filePath,
+      babelrc: false,
+      configFile: false,
+      plugins: [
+        [compiledBabelPlugin, { onIncludedFile: (file: string) => includedFiles.push(file) }],
+      ],
+      caller: {
+        name: 'compiled',
+      },
+    });
+
+    includedFiles.forEach((file) => {
+      asset.addIncludedFile(file);
+    });
+
+    if (result?.ast) {
+      asset.setAST({
+        type: 'compiled',
+        version: '0.0.0',
+        program: result.ast,
       });
-
-      if (result?.ast) {
-        asset.setAST({
-          type: 'babel',
-          version: '7.0.0',
-          program: result.ast,
-        });
-      }
     }
 
     return [asset];
   },
 
-  async generate({ asset, ast, options }: any) {
-    return generate({ asset, ast, options });
+  generate({ ast }: any) {
+    const { code, map } = generate(ast.program, {});
+
+    return { code, map };
   },
 });
