daf06a24a8321d115d9b980de059d63132b627fc
madou <laheen@gmail.com>
2021-02-27 09:58:38 +1100

    fix: code to content

--- packages/parcel-transformer/src/index.tsx
+++ packages/parcel-transformer/src/index.tsx
@@ -1,5 +1,4 @@
 import { Transformer } from '@parcel/plugin';
-import semver from 'semver';
 import compiledBabelPlugin from '@compiled/babel-plugin';
 import { parseAsync, transformFromAstAsync } from '@babel/core';
 import generate from '@babel/generator';
@@ -8,19 +7,25 @@ import generate from '@babel/generator';
  * Compiled parcel transformer.
  */
 export default new Transformer({
-  canReuseAST({ ast }: any) {
-    return ast.type === 'babel' && semver.satisfies(ast.version, '^7.0.0');
+  canReuseAST() {
+    // Compiled should run before any other JS transformer.
+    return false;
   },
 
   async parse({ asset }: any) {
     const code = await asset.getCode();
     if (code.indexOf('@compiled/react') === -1) {
+      // We only want to parse files that are actually using Compiled.
+      // For everything else we bail out.
       return undefined;
     }
 
     const ast = await parseAsync(code, {
       filename: asset.filePath,
       caller: { name: 'compiled' },
+      // TODO: We want to inherit user land configuration for the initial parse,
+      // but Parcel also ships with a "default" config. We should figure out if we
+      // can grab theirs.
       plugins: ['@babel/plugin-syntax-jsx'],
     });
 
@@ -29,7 +34,8 @@ export default new Transformer({
 
   async transform({ asset, ast }: any) {
     if (!asset.isSource || !ast) {
-      // We're not interested in this asset.
+      // We will only recieve asts for assets we're interested in.
+      // Since this is undefined we aren't interested in it.
       return [asset];
     }
 
@@ -43,7 +49,10 @@ export default new Transformer({
       babelrc: false,
       configFile: false,
       plugins: [
-        [compiledBabelPlugin, { onIncludedFile: (file: string) => includedFiles.push(file) }],
+        [
+          compiledBabelPlugin,
+          { onIncludedFile: (file: string) => includedFiles.push(file), cache: false },
+        ],
       ],
       caller: {
         name: 'compiled',
@@ -56,6 +65,8 @@ export default new Transformer({
 
     if (result?.ast) {
       asset.setAST({
+        // TODO: Currently if we set this as `'babel'` the babel transformer blows up.
+        // Let's figure out what we can do to reuse it.
         type: 'compiled',
         version: '0.0.0',
         program: result.ast,
@@ -65,9 +76,11 @@ export default new Transformer({
     return [asset];
   },
 
-  generate({ ast }: any) {
-    const { code, map } = generate(ast.program, {});
+  generate({ ast, asset }: any) {
+    const { code, map } = generate(ast.program, {
+      filename: asset.filePath,
+    });
 
-    return { code, map };
+    return { content: code, map };
   },
 });
