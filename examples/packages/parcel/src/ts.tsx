2be87a37d40ab3ec9a86453b45f2ee4b236dd050
madou <laheen@gmail.com>
2021-02-27 13:59:18 +1100

    chore: adds ts example

---
+++ examples/packages/parcel/src/ts.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import '@compiled/react';
+import { primary } from './module';
+
+interface TSProps {
+  color: string;
+}
+
+export default function TypeScriptHome({ color }: TSProps) {
+  return (
+    <div
+      css={{
+        display: 'inline-block',
+        padding: 8,
+        border: `2px solid ${primary}`,
+        borderRadius: 3,
+        color,
+        textTransform: 'uppercase',
+      }}>
+      hello from typescript file
+    </div>
+  );
+}
