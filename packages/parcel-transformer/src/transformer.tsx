d8e21e69e618d5f26e83efcb408608fd89879a7a
madou <laheen@gmail.com>
2021-03-01 19:37:42 +1100

    chore: small refactor

--- packages/parcel-transformer/src/transformer.tsx
+++ packages/parcel-transformer/src/transformer.tsx
@@ -5,17 +5,21 @@ import generate from '@babel/generator';
 
 type UserlandOpts = Omit<PluginOptions, 'cache' | 'onIncludedFile'>;
 
+const configFiles = [
+  '.compiledcssrc',
+  '.compiledcssrc.json',
+  'compiledcss.js',
+  'compiledcss.config.js',
+];
+
 /**
  * Compiled parcel transformer.
  */
 export default new Transformer<UserlandOpts>({
   async loadConfig({ config }) {
-    const conf = await config.getConfig(
-      ['.compiledcssrc', '.compiledcssrc.json', 'compiledcss.js', 'compiledcss.config.js'],
-      {
-        packageKey: 'compiled',
-      }
-    );
+    const conf = await config.getConfig(configFiles, {
+      packageKey: '@compiled/parcel-transformer',
+    });
 
     const contents: UserlandOpts = {};
 
@@ -34,8 +38,12 @@ export default new Transformer<UserlandOpts>({
   },
 
   async parse({ asset }) {
+    if (!asset.isSource) {
+      return undefined;
+    }
+
     const code = await asset.getCode();
-    if (!asset.isSource || code.indexOf('@compiled/react') === -1) {
+    if (code.indexOf('@compiled/react') === -1) {
       // We only want to parse files that are actually using Compiled.
       // For everything else we bail out.
       return undefined;
@@ -66,6 +74,7 @@ export default new Transformer<UserlandOpts>({
       filename: asset.filePath,
       babelrc: false,
       configFile: false,
+      sourceMaps: true,
       plugins: [
         [
           '@compiled/babel-plugin',
@@ -103,7 +112,7 @@ export default new Transformer<UserlandOpts>({
 
   generate({ ast, asset }) {
     // TODO: We're using babels standard generator. Internally parcel does some hacks in
-    // the official babel transformer to make it faster - using ASTree directly.
+    // the official Babel transformer to make it faster - using ASTree directly.
     // Perhaps we should do the same thing.
     const { code, map } = generate(ast.program, {
       filename: asset.filePath,
