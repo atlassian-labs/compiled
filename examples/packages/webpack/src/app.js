273b381c970f9d6b310d1519261220cda4a93c3f
Michael Dougall <mdougall@atlassian.com>
2021-03-11 10:31:14 +1100

    chore: adds async component

--- examples/packages/webpack/src/app.js
+++ examples/packages/webpack/src/app.js
@@ -1,12 +1,18 @@
+import { Suspense, lazy } from 'react';
 import '@compiled/react';
 import { primary } from './module';
 import HelloWorld from './component';
+
+const AsyncComponent = lazy(() => import('./async'));
 
 export default function Home() {
   return (
     <>
       <div css={{ fontSize: 50, color: primary }}>hello from webpack</div>
       <HelloWorld>TypeScript component</HelloWorld>
+      <Suspense fallback="Loading...">
+        <AsyncComponent>I was loaded async</AsyncComponent>
+      </Suspense>
     </>
   );
 }
---
+++ examples/packages/webpack/src/async.js
@@ -0,0 +1,9 @@
+import { styled } from '@compiled/react';
+
+const LoadedAsync = styled.div`
+  font-weight: 700;
+  color: purple;
+  border: 2px solid pink;
+`;
+
+export default LoadedAsync;
