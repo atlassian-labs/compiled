bbeea103041a2e79acecf981c7eddee06ad4e24a
andparsons <andparsons@gmail.com>
2021-10-01 04:06:10 +0100

    Update transformer type to inherit from @parcel/types

--- packages/parcel-transformer/types/parcel__plugin.d.ts
+++ packages/parcel-transformer/types/parcel__plugin.d.ts
@@ -1,37 +1,7 @@
 declare module '@parcel/plugin' {
-  interface Asset {
-    isSource: boolean;
-    filePath: string;
-    isASTDirty(): boolean;
-    addIncludedFile(file: string): void;
-    getCode(): Promise<string>;
-    getAST(): Promise<any>;
-    setAST(opts: { type: string; version: string; program: any }): void;
-  }
-
-  interface ConfigResult<TConfig extends unknown> {
-    contents: TConfig;
-    filePath: string;
-  }
-
-  interface ConfigSetup<TConfig extends unknown> {
-    shouldInvalidateOnStartup(): void;
-    setResult(opts: TConfig): void;
-    getConfig(
-      files: string[],
-      opts: { packageKey: string }
-    ): Promise<ConfigResult<TConfig> | undefined>;
-  }
-
-  interface TransformerOpts<TConfig extends unknown> {
-    canReuseAST(opts: { asset: Asset; config: TConfig }): boolean;
-    parse(opts: { asset: Asset; config: TConfig }): Promise<any>;
-    transform(opts: { asset: Asset; config: TConfig }): Promise<any[]>;
-    generate(opts: { asset: Asset; ast: any }): { content: string; map: any };
-    loadConfig(opts: { config: ConfigSetup<TConfig> }): Promise<void>;
-  }
+  import type { Transformer as T } from '@parcel/types';
 
   export class Transformer<TConfig extends unknown> {
-    constructor(opts: TransformerOpts<TConfig>);
+    constructor(opts: T<TConfig>);
   }
 }
