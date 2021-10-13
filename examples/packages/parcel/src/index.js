2e98a2668c124c65ee35cd86f6827ea6b8eefe66
Monica Olejniczak <molejniczak@atlassian.com>
2021-10-13 12:01:11 +1100

    Add placeholder parcel resolution examples

--- examples/packages/parcel/src/index.js
+++ examples/packages/parcel/src/index.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
-import App from './app';
+import { App } from './app';
 
-render(<App />, document.getElementById('root'));
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
