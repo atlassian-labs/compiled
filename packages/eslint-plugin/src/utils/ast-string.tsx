da97153d7241dc4569607bc5fbf0f2efa6bd0908
Michael Dougall <mdougall@atlassian.com>
2021-11-21 08:08:57 +1100

    chore: adds jsdoc

--- packages/eslint-plugin/src/utils/ast-string.tsx
+++ packages/eslint-plugin/src/utils/ast-string.tsx
@@ -1,8 +1,20 @@
 import type { ImportSpecifier, ImportDefaultSpecifier, ImportNamespaceSpecifier } from 'estree';
 
+/**
+ * Builds a string representation of an import declaration.
+ *
+ * @param innerNodeStr Contents to place between the import brackets
+ * @param module Module name to import from
+ */
 export const buildImportDeclaration = (innerNodeStr: string, module: string): string =>
   `import { ${innerNodeStr} } from '${module}';`;
 
+/**
+ * Builds a string representation of a named import.
+ * If the node has a local name will rebind it to that.
+ *
+ * @param node
+ */
 export const buildNamedImport = (
   node: ImportSpecifier | ImportDefaultSpecifier | ImportNamespaceSpecifier
 ): string => {
