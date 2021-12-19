0f05c3863c805fbb309d2ed88ce60e8326a91e68
Michael Dougall <mdougall@atlassian.com>
2021-12-19 12:30:36 +1100

    feat: adds custom entrypoints

--- test/test-imports.js
+++ test/test-imports.js
@@ -2,3 +2,4 @@ require('@compiled/react');
 require('@compiled/react/runtime');
 require('@compiled/dom__experimental');
 require('@compiled/babel-plugin');
+require('@compiled/babel-plugin/dom__experimental');
