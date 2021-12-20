9192f86538a8d91884a431c7f7c003fc00e55a4f
Michael Dougall <mdougall@atlassian.com>
2021-12-20 16:42:48 +1100

    fix: conditionally use entrypoint when dist is built

--- packages/babel-plugin/dom__experimental.js
+++ packages/babel-plugin/dom__experimental.js
@@ -1,18 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const tsNode = require('ts-node');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const tsPaths = require('tsconfig-paths');
+const fs = require('fs');
 
-tsNode.register({
-  transpileOnly: true,
-  project: __dirname + '/tsconfig.json',
-});
+if (fs.existsSync(__dirname + '/dist')) {
+  module.exports = require('./dist/dom__experimental');
+} else {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const tsNode = require('ts-node');
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const tsPaths = require('tsconfig-paths');
 
-tsPaths.register();
+  tsNode.register({
+    transpileOnly: true,
+    project: __dirname + '/tsconfig.json',
+  });
 
-/**
- * This exists for local sessions so source files don't need to be built.
- * This is an unfortunate hack if you're reading this and have a spare
- * eng health week please find a better way to not have to do this.
- */
-module.exports = require('./src/dom__experimental');
+  tsPaths.register();
+
+  /**
+   * This exists for local sessions so source files don't need to be built.
+   * This is an unfortunate hack if you're reading this and have a spare
+   * eng health week please find a better way to not have to do this.
+   */
+  module.exports = require('./src/dom__experimental');
+}
