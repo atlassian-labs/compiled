babe569db774f221fa0b3012075ae8a7e6a747ff
Wilson Hou <whou@atlassian.com>
2022-01-10 16:09:23 +1100

    Added ImportDeclarationWithInnerComments Type

--- packages/codemods/src/plugins/default.ts
+++ packages/codemods/src/plugins/default.ts
@@ -1,29 +1,48 @@
-import type { JSXAttribute } from 'jscodeshift';
+import type { CommentBlock, CommentLine, JSXAttribute, ImportDeclaration } from 'jscodeshift';
 
 import type { CodemodPlugin } from './types';
+
+// This is required since the imported ImportDeclaration is incorrectly typed (does not have an innerComments property.)
+export interface ImportDeclarationWithInnerComments extends ImportDeclaration {
+  innerComments?: (CommentLine | CommentBlock)[];
+}
 
 const defaultCodemodPlugin: CodemodPlugin = {
   name: 'default-plugin',
   create: (_, { jscodeshift: j }) => ({
     transform: {
       buildImport({ compiledImportPath, currentNode, specifiers }) {
-        const newImport = j.importDeclaration(specifiers, j.literal(compiledImportPath));
-
-        // * Should we make a function to copy over commentLine vs commentBlock??
+        const newImport: ImportDeclarationWithInnerComments = j.importDeclaration(
+          specifiers,
+          j.literal(compiledImportPath)
+        );
 
         // ! Note: Comments are copied over imperfectly.
         // This is due to open recast Issue #191 where trailing comments are being turned into leading comments. Link: https://github.com/benjamn/recast/issues/191
         // Also, jscodeshift does not accept location information for comments.
         // Expect inline comments to be bumped up one row.
-        // TODO: Fix stupid type issue where ImportDeclaration for some reason doesn't recognise innerComments as a property. ... wait nvm it does??
-        // ! Issue: Node has .comments > Statement > Declaration > ... everything else
-        // ^ This should be .innerComments
-        // ? Fix: declare / extend a type (Node? etc.) to handle innerComments.
-        // * Blocker: idk how to do this - ASK JAKE OR (Nathan)
+
+        // ? Named to Default Specifier
+        // ? Make comments consistent - formatted (maybe copy over source? idk)
 
         // TODO: Copy over innerComments.
+        (currentNode as ImportDeclarationWithInnerComments)?.innerComments?.forEach(
+          (innerComment) => {
+            if (!newImport.innerComments) {
+              newImport.innerComments = [];
+            }
 
-        // ! newImport.innerComments = currentNode.comments;
+            innerComment.type == 'CommentLine'
+              ? newImport.innerComments.push(
+                  // j.commentLine(innerComment.value, innerComment.leading, innerComment.trailing)
+                  j.commentLine(innerComment.value, true, false)
+                )
+              : newImport.innerComments.push(
+                  // j.commentBlock(innerComment.value, innerComment.leading, innerComment.trailing)
+                  j.commentBlock(innerComment.value, true, false)
+                );
+          }
+        );
 
         // TODO: Copy over comments that live in specifiers.
         currentNode.specifiers?.forEach((specifier, idx) => {
@@ -39,11 +58,13 @@ const defaultCodemodPlugin: CodemodPlugin = {
             comment.type == 'CommentLine'
               ? newImport?.specifiers?.[idx]?.comments?.push(
                   // Handle Inline Comment Case
-                  j.commentLine(comment.value, comment.leading, comment.trailing)
+                  // j.commentLine(comment.value, comment.leading, comment.trailing)
+                  j.commentLine(comment.value, true, false)
                 )
               : newImport?.specifiers?.[idx]?.comments?.push(
                   // Handle Inline Comment Case
-                  j.commentBlock(comment.value, comment.leading, comment.trailing)
+                  // j.commentBlock(comment.value, comment.leading, comment.trailing)
+                  j.commentBlock(comment.value, true, false)
                 );
           });
         });
