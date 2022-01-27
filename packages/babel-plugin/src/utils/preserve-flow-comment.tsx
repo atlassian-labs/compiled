c8dee89354ee3dfd8a0456e3459e1c5515f91f41
Celeste Carloni <celestecarloni@gmail.com>
2022-01-27 17:25:29 +1000

    fix: prettier

--- packages/babel-plugin/src/utils/preserve-flow-comment.tsx
+++ packages/babel-plugin/src/utils/preserve-flow-comment.tsx
@@ -2,20 +2,19 @@ import type { NodePath } from '@babel/traverse';
 import type * as t from '@babel/types';
 
 /**
- * Ensure that any flow comment is preserved at the top of the processed file before additional imports are 
- * dynamically inserted during the build. 
+ * Ensure that any flow comment is preserved at the top of the processed file before additional imports are
+ * dynamically inserted during the build.
  *
  * @param path
  */
- export const preserveFlowComment = (path: NodePath<t.Program>): void => {
-
-    const leadingComment = path.node.body.length > 0 &&
+export const preserveFlowComment = (path: NodePath<t.Program>): void => {
+  const leadingComment =
+    path.node.body.length > 0 &&
     path.node.body[0].leadingComments &&
     path.node.body[0].leadingComments.length > 0 &&
     path.node.body[0].leadingComments[0];
 
-    if(leadingComment && leadingComment.value.indexOf('@flow') !== -1) {
-        path.addComment('leading', leadingComment.value);
-    }
-}
-
+  if (leadingComment && leadingComment.value.indexOf('@flow') !== -1) {
+    path.addComment('leading', leadingComment.value);
+  }
+};
