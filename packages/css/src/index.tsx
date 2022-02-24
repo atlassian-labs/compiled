0653dfaaeceb6daaa39e3b5c301213441cfcc34e
Celeste Carloni <celestecarloni@gmail.com>
2022-02-24 11:05:55 +1000

    fix: remove export  not longer needed

--- packages/css/src/index.tsx
+++ packages/css/src/index.tsx
@@ -2,7 +2,6 @@ export { addUnitIfNeeded } from './utils/css-property';
 export { transformCss } from './transform';
 export {
   cssAffixInterpolation,
-  cssAfterInterpolation,
   AfterInterpolation,
   BeforeInterpolation,
 } from './utils/css-affix-interpolation';
