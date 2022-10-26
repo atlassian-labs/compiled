3d4002d2951fef2f43f4c38baf83110e95d25f93
Liam Ma <lma2@atlassian.com>
2022-10-26 15:11:32 +1100

    Add missing type file

---
+++ packages/parcel-optimizer/src/types.ts
@@ -0,0 +1,7 @@
+export interface ParcelOptimizerOpts {
+  /**
+   * Indicates whether CSS content is inlined in HTML or served as a external .css file.
+   * Defaults to `false`.
+   */
+  inlineCSS: boolean;
+}
