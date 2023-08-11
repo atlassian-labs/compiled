9cfda8ef7493c769c2cf84ec682f0d6277f80eec
Grant Wong <2908767+dddlr@users.noreply.github.com>
2023-08-11 16:52:37 +1200

    Forbid imported styles and function parameters in css attribute (#1491)
    * Add import and function parameters to the scope of no-css-prop-without-css-function
    
    * Add support for 'as const'
    
    * Migrate eslint rule to typescript
    
    * Add documentation
    
    * Handle non-deconstructed parameter properties
    
    * Refactor no-css-prop-without-css-function

--- packages/eslint-plugin/src/rules/no-css-prop-without-css-function/index.ts
+++ packages/eslint-plugin/src/rules/no-css-prop-without-css-function/index.ts
@@ -1,17 +1,129 @@
-import type { Rule, Scope } from 'eslint';
-import type { Node } from 'estree';
-import type { JSXExpressionContainer } from 'estree-jsx';
+import type { TSESTree, TSESLint } from '@typescript-eslint/utils';
 
-import { findCompiledImportDeclarations } from '../../utils/ast';
+import {
+  findTSCompiledImportDeclarations,
+  isDOMElement,
+  traverseUpToJSXOpeningElement,
+} from '../../utils/ast';
 import { addImportToDeclaration, buildImportDeclaration } from '../../utils/ast-to-string';
 
-type Q<T> = T extends Scope.Definition ? (T['type'] extends 'Variable' ? T : never) : never;
-type VariableDefinition = Q<Scope.Definition>;
+type Q<T> = T extends TSESLint.Scope.Definition
+  ? T['type'] extends 'Variable'
+    ? T
+    : never
+  : never;
+type VariableDefinition = Q<TSESLint.Scope.Definition>;
+type ParameterDefinition = TSESLint.Scope.Definitions.ParameterDefinition;
 
-const findStyleNodes = (node: Node, references: Scope.Reference[], context: Rule.RuleContext) => {
+type CSSValue = TSESTree.Expression | TSESTree.JSXEmptyExpression;
+type Reference = TSESLint.Scope.Reference;
+type Context = TSESLint.RuleContext<string, readonly []>;
+
+const findNodeReference = (
+  references: Reference[],
+  node: TSESTree.Expression
+): Reference | undefined => {
+  return references.find((reference) => reference.identifier === node);
+};
+
+const handleIdentifier = (node: TSESTree.Identifier, references: Reference[], context: Context) => {
+  // Resolve the variable for the reference
+  const reference = findNodeReference(references, node);
+  const definition = reference?.resolved?.defs.find(
+    (def): def is VariableDefinition => def.type === 'Variable'
+  );
+
+  // Traverse to the variable value
+  if (definition && definition.node.init) {
+    findStyleNodes(definition.node.init, references, context);
+  } else {
+    const isImported = reference?.resolved?.defs.find((def) => def.type === 'ImportBinding');
+    const isFunctionParameter = reference?.resolved?.defs.find((def) => def.type === 'Parameter');
+
+    const jsxElement = traverseUpToJSXOpeningElement(node);
+
+    // css property on DOM elements are always fine, e.g.
+    // <div css={...}> instead of <MyComponent css={...}>
+    if (jsxElement.name.type === 'JSXIdentifier' && isDOMElement(jsxElement.name.name)) {
+      return;
+    }
+
+    if (isImported) {
+      context.report({
+        messageId: 'importedInvalidCssUsage',
+        node,
+      });
+    } else if (isFunctionParameter) {
+      context.report({
+        messageId: 'functionParameterInvalidCssUsage',
+        node,
+      });
+    } else {
+      context.report({
+        messageId: 'otherInvalidCssUsage',
+        node,
+      });
+    }
+  }
+};
+
+const handleMemberExpression = (
+  node: TSESTree.MemberExpression,
+  references: Reference[],
+  context: Context
+) => {
+  const reference = findNodeReference(references, node.object);
+  const definition = reference?.resolved?.defs.find(
+    (def): def is ParameterDefinition => def.type === 'Parameter'
+  );
+
+  if (definition) {
+    context.report({
+      messageId: 'functionParameterInvalidCssUsage',
+      node,
+    });
+  }
+};
+
+const fixWrapper = (node: CSSValue, context: Context) => {
+  function* fix(fixer: TSESLint.RuleFixer) {
+    const compiledImports = findTSCompiledImportDeclarations(context);
+    const source = context.getSourceCode();
+
+    if (compiledImports.length > 0) {
+      // Import found, add the specifier to it
+      const [firstCompiledImport] = compiledImports;
+      const specifiersString = addImportToDeclaration(firstCompiledImport, ['css']);
+
+      yield fixer.replaceText(firstCompiledImport, specifiersString);
+    } else {
+      // Import not found, add a new one
+      yield fixer.insertTextAfter(
+        source.ast.body[0],
+        `\n${buildImportDeclaration('css', '@compiled/react')}`
+      );
+    }
+
+    if (node.type === 'ObjectExpression') {
+      const parent = node.parent;
+      if (parent && parent.type === 'TSAsExpression') {
+        yield fixer.replaceText(parent, `css(${source.getText(node)})`);
+      } else {
+        yield fixer.insertTextBefore(node, 'css(');
+        yield fixer.insertTextAfter(node, ')');
+      }
+    } else {
+      yield fixer.insertTextBefore(node, 'css');
+    }
+  }
+
+  return fix;
+};
+
+const findStyleNodes = (node: CSSValue, references: Reference[], context: Context): void => {
   if (node.type === 'ArrayExpression') {
     node.elements.forEach((arrayElement) => {
-      if (arrayElement) {
+      if (arrayElement && arrayElement.type !== 'SpreadElement') {
         findStyleNodes(arrayElement, references, context);
       }
     });
@@ -24,70 +136,55 @@ const findStyleNodes = (node: Node, references: Scope.Reference[], context: Rule
     findStyleNodes(node.consequent, references, context);
     findStyleNodes(node.alternate, references, context);
   } else if (node.type === 'Identifier') {
-    // Resolve the variable for the reference
-    const reference = references.find((reference) => reference.identifier === node);
-    const definition = reference?.resolved?.defs.find(
-      (def): def is VariableDefinition => def.type === 'Variable'
-    );
-
-    // Traverse to the variable value
-    if (definition && definition.node.init) {
-      findStyleNodes(definition.node.init, references, context);
-    }
+    handleIdentifier(node, references, context);
   } else if (node.type === 'MemberExpression') {
-    // Since we don't support MemberExpression yet, we don't have a contract for what it should look like
-    // We can skip this for now, until we implement the CSS map API
+    handleMemberExpression(node, references, context);
   } else if (node.type === 'ObjectExpression' || node.type === 'TemplateLiteral') {
     // We found an object expression that was not wrapped, report
     context.report({
       messageId: 'noCssFunction',
       node,
-      *fix(fixer) {
-        const compiledImports = findCompiledImportDeclarations(context);
-        if (compiledImports.length > 0) {
-          // Import found, add the specifier to it
-          const [firstCompiledImport] = compiledImports;
-          const specifiersString = addImportToDeclaration(firstCompiledImport, ['css']);
-
-          yield fixer.replaceText(firstCompiledImport, specifiersString);
-        } else {
-          // Import not found, add a new one
-          const source = context.getSourceCode();
-          yield fixer.insertTextAfter(
-            source.ast.body[0],
-            `\n${buildImportDeclaration('css', '@compiled/react')}`
-          );
-        }
-
-        if (node.type === 'ObjectExpression') {
-          yield fixer.insertTextBefore(node, 'css(');
-          yield fixer.insertTextAfter(node, ')');
-        } else {
-          yield fixer.insertTextBefore(node, 'css');
-        }
-      },
+      fix: fixWrapper(node, context),
     });
+  } else if (node.type === 'TSAsExpression') {
+    // TSAsExpression is anything in the form "X as Y", e.g.:
+    // const abc = { ... } as const;
+    return findStyleNodes(node.expression, references, context);
   }
 };
 
-const createNoCssPropWithoutCssFunctionRule = (): Rule.RuleModule['create'] => (context) => ({
-  'JSXAttribute[name.name="css"] JSXExpressionContainer': (node: Rule.Node): void => {
-    const { references } = context.getScope();
+const createNoCssPropWithoutCssFunctionRule =
+  (): TSESLint.RuleModule<string>['create'] => (context) => ({
+    'JSXAttribute[name.name="css"] JSXExpressionContainer': (
+      node: TSESTree.JSXExpressionContainer
+    ): void => {
+      const { references } = context.getScope();
 
-    findStyleNodes((node as JSXExpressionContainer).expression, references, context);
-  },
-});
+      findStyleNodes(node.expression, references, context);
+    },
+  });
 
-export const noCssPropWithoutCssFunctionRule: Rule.RuleModule = {
+export const noCssPropWithoutCssFunctionRule: TSESLint.RuleModule<string> = {
+  defaultOptions: [],
   meta: {
     docs: {
       url: 'https://github.com/atlassian-labs/compiled/tree/master/packages/eslint-plugin/src/rules/no-css-prop-without-css-function',
+      recommended: 'error',
+      description:
+        'Disallows `css` prop usages without wrapping in the `css` import from `@compiled/react`. Also forbids `css` prop usages where Compiled cannot determine whether the `css` import is included at build time.',
     },
     messages: {
       noCssFunction: 'css prop values are required to use the css import from @compiled/react',
+      importedInvalidCssUsage:
+        'Compiled cannot determine the value of imported values in the css attribute at build time. If this component uses Compiled, this will cause a build error or invalid CSS! Consider moving the value into the same file.',
+      functionParameterInvalidCssUsage:
+        'Compiled cannot determine the value of function props in the css attribute at build time. If this component uses Compiled, this will cause a build error or invalid CSS! Consider moving the value into the same file.',
+      otherInvalidCssUsage:
+        'Compiled cannot determine the value of this expression in the css attribute at build time. If this component uses Compiled, this will cause a build error or invalid CSS! Consider moving the value into the same file.',
     },
     type: 'problem',
     fixable: 'code',
+    schema: [],
   },
   create: createNoCssPropWithoutCssFunctionRule(),
 };
--- packages/eslint-plugin/src/utils/ast.ts
+++ packages/eslint-plugin/src/utils/ast.ts
@@ -1,3 +1,4 @@
+import type { TSESTree, TSESLint } from '@typescript-eslint/utils';
 import type { Rule } from 'eslint';
 import type { ImportDeclaration, ImportSpecifier } from 'estree';
 
@@ -16,6 +17,55 @@ export const findCompiledImportDeclarations = (context: Rule.RuleContext): Impor
       (node): node is ImportDeclaration =>
         node.type === 'ImportDeclaration' && node.source.value === COMPILED_IMPORT
     );
+};
+
+/**
+ * Re-implementation of findCompiledImportDeclarations for typescript-eslint.
+ *
+ * Given a rule, return any `@compiled/react` import declarations in the source code.
+ *
+ * @param context Rule context
+ * @returns a list of import declarations
+ */
+export const findTSCompiledImportDeclarations = (
+  context: TSESLint.RuleContext<string, readonly unknown[]>
+): TSESTree.ImportDeclaration[] => {
+  return context
+    .getSourceCode()
+    .ast.body.filter(
+      (node): node is TSESTree.ImportDeclaration =>
+        node.type === 'ImportDeclaration' && node.source.value === COMPILED_IMPORT
+    );
+};
+
+/**
+ * Returns whether the element is a DOM element, which is all lowercase...
+ * as opposed to a React component, which is capitalized.
+ *
+ * @param elementName
+ * @returns whether the element is a DOM element (true) or a React component (false)
+ */
+export const isDOMElement = (elementName: string): boolean =>
+  elementName.charAt(0) !== elementName.charAt(0).toUpperCase() &&
+  elementName.charAt(0) === elementName.charAt(0).toLowerCase();
+
+/**
+ * Traverses up the AST until it reaches a JSXOpeningElement. Used in conjunction with
+ * isDOMElement to detect whether the enclosing element is a DOM element or not.
+ *
+ * @param node
+ * @returns a JSXOpeningElement
+ */
+export const traverseUpToJSXOpeningElement = (node: TSESTree.Node): TSESTree.JSXOpeningElement => {
+  while (node.parent && node.type !== 'JSXOpeningElement') {
+    return traverseUpToJSXOpeningElement(node.parent);
+  }
+
+  if (node.type === 'JSXOpeningElement') {
+    return node;
+  }
+
+  throw new Error('Could not find JSXOpeningElement');
 };
 
 /**
