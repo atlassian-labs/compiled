db26a778998e169b0020f521876986628210ac06
Liam Ma <liam.q.ma@gmail.com>
2023-09-18 13:48:12 +1000

    Update webpack plugin

--- examples/webpack/src/app.jsx
+++ examples/webpack/src/app.jsx
@@ -1,5 +1,6 @@
+import BabelComponentExtracted from '@compiled/babel-component-extracted-fixture/dist/index';
 import BabelComponent from '@compiled/babel-component-fixture';
-import '@compiled/react';
+import { css } from '@compiled/react';
 import { Suspense, lazy } from 'react';
 
 import { primary } from './common/constants';
@@ -15,12 +16,13 @@ const AsyncComponent = lazy(() => import('./ui/async'));
 
 export const App = () => (
   <>
-    <div css={{ fontSize: 50, color: primary }}>hello from webpack</div>
+    <div css={css({ fontSize: 50, color: primary })}>hello from webpack</div>
     <TypeScript>TypeScript component</TypeScript>
     <BabelComponent>Component from NPM</BabelComponent>
     <Suspense fallback="Loading...">
       <AsyncComponent>I was loaded async</AsyncComponent>
     </Suspense>
+    <BabelComponentExtracted>Component from NPM</BabelComponentExtracted>
     <CustomFileExtensionStyled>Custom File Extension Styled</CustomFileExtensionStyled>
     <div css={customFileExtensionCss}>Custom File Extension CSS</div>
     <Annotated />
