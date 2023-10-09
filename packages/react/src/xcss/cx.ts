3f5197423941f4c71d0f33d6eb1d21f8f57b281a
Alex Hinds <ahinds@atlassian.com>
2023-10-10 09:56:45 +1100

    chore: update docs

--- packages/react/src/xcss/cx.ts
+++
@@ -1,9 +0,0 @@
-import type { XCSSProp } from './xcss';
-
-const cx = <Collection extends [...XCSSProp<any, any>[]]>(
-  ..._css: Collection
-): Collection[number] => {
-  throw new Error('invariant');
-};
-
-export default cx;
