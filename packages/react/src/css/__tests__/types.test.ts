35dff54cfa192e7e91efd982c7928298649e67e0
Michael Dougall <mdougall@atlassian.com>
2023-10-10 10:29:06 +1100

    chore: fix import/no-extraneous-dependencies rule

--- packages/react/src/css/__tests__/types.test.ts
+++ packages/react/src/css/__tests__/types.test.ts
@@ -1,6 +1,5 @@
 /** @jsxImportSource @compiled/react */
 import { css } from '@compiled/react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { expectTypeOf } from 'expect-type';
 
 describe('css func tests', () => {
