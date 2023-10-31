d7f960732f2449a1d71c8e9cf2ed58482d2b2776
Michael Dougall <mdougall@atlassian.com>
2023-10-31 14:42:41 +1100

    feat: add no-suppress-xcss rule logic

--- packages/eslint-plugin/src/rules/no-suppress-xcss/__tests__/rule.test.ts
+++ packages/eslint-plugin/src/rules/no-suppress-xcss/__tests__/rule.test.ts
@@ -2,6 +2,101 @@ import { typeScriptTester as tester } from '../../../test-utils';
 import { noSuppressXCSS } from '../index';
 
 tester.run('no-styled-tagged-template-expression', noSuppressXCSS, {
-  valid: [],
-  invalid: [],
+  valid: [
+    `
+      <Component xcss={{ fill: 'red' }} />
+    `,
+    `
+      <Component innerXcss={{ fill: 'red' }} />
+    `,
+    `
+      // @ts-expect-error
+      function Foo() {
+        // @ts-ignore
+        return (
+          <>
+            <Component xcss={{ fill: 'red' }} />
+          </>
+        );
+      }
+    `,
+  ],
+  invalid: [
+    {
+      code: `
+      // @ts-expect-error
+      <Component xcss={{ fill: 'red' }} />
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+    {
+      code: `
+      // @ts-expect-error
+      <Component innerXcss={{ fill: 'red' }} />
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+    {
+      code: `
+      // @ts-ignore
+      <Component xcss={{ fill: 'red' }} />
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+    {
+      code: `
+
+      <Component
+        // @ts-expect-error
+        innerXcss={{ fill: 'red' }}
+      />
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+    {
+      code: `
+
+      <Component
+        // @ts-ignore
+        xcss={{ fill: 'red' }}
+      />
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+    {
+      code: `
+      // @ts-ignore
+      <Component innerXcss={{ fill: 'red' }} />
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+    {
+      only: true,
+      code: `
+      function Foo() {
+        return (
+          <>
+            {/* @ts-ignore */}
+            <Component xcss={{ fill: 'red' }} />
+          </>
+        );
+      }
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+    {
+      only: true,
+      code: `
+      function Foo() {
+        return (
+          <>
+            {/* @ts-expect-error */}
+            <Component xcss={{ fill: 'red' }} />
+          </>
+        );
+      }
+    `,
+      errors: [{ messageId: 'no-suppress-xcss' }],
+    },
+  ],
 });
--- packages/eslint-plugin/src/rules/no-suppress-xcss/index.ts
+++ packages/eslint-plugin/src/rules/no-suppress-xcss/index.ts
@@ -8,10 +8,41 @@ export const noSuppressXCSS: Rule.RuleModule = {
         'The xcss prop is predicated on adhering to the type contract. Supressing it breaks this contract and thus is not allowed.',
       url: 'https://github.com/atlassian-labs/compiled/tree/master/packages/eslint-plugin/src/rules/no-supress-xcss',
     },
-    messages: {},
+    messages: {
+      'no-suppress-xcss': 'bad!',
+    },
     type: 'problem',
   },
-  create() {
-    return {};
+  create(context) {
+    return {
+      'JSXAttribute[name.name=/[xX]css$/]': (node: Rule.Node) => {
+        if (!node.loc) {
+          return;
+        }
+
+        const comments = context.sourceCode.getAllComments();
+
+        for (let i = 0; i < comments.length; i++) {
+          const comment = comments[i];
+          if (!comment.loc) {
+            continue;
+          }
+
+          const commentLine = comment.loc.start.line;
+          const nodeLine = node.loc.start.line;
+          const isCommentOnPreviousLine = nodeLine - 1 === commentLine;
+
+          if (
+            isCommentOnPreviousLine &&
+            ['@ts-expect-error', '@ts-ignore'].some((tag) => comment.value.includes(tag))
+          ) {
+            context.report({
+              node,
+              messageId: 'no-suppress-xcss',
+            });
+          }
+        }
+      },
+    };
   },
 };
