28559a540207e23cc9cdd2be14b03ece0652f03d
Grant Wong <2908767+dddlr@users.noreply.github.com>
2023-11-07 14:20:13 +1100

    Update no-exported-css and no-exported-keyframes error message (#1552)

--- packages/eslint-plugin/src/rules/no-exported-keyframes/index.ts
+++ packages/eslint-plugin/src/rules/no-exported-keyframes/index.ts
@@ -8,7 +8,8 @@ export const noExportedKeyframesRule: Rule.RuleModule = {
       url: 'https://github.com/atlassian-labs/compiled/tree/master/packages/eslint-plugin/src/rules/no-exported-css',
     },
     messages: {
-      unexpected: 'Unexpected `keyframes` export declaration from @compiled/react',
+      unexpected:
+        "`keyframes` can't be exported - this will cause unexpected behaviour at runtime. Instead, please move your `keyframes(...)` code to the same file where these styles are being used.",
     },
     type: 'problem',
   },
