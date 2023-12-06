b5325ed6f1ddb66d31050f94a291168c488b1626
Jake Lane <jlane2@atlassian.com>
2023-12-07 10:32:14 +1100

    Revert "Fix jsx pragma breaking @compiled/babel-plugin-strip-runtime stylesheet extraction (#1557)"
    This reverts commit fbc17ed3d455df825127f951d296f49c384cf4be.

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,5 @@
 export { flatten, unique } from './array';
 export { hash } from './hash';
-export { JSX_ANNOTATION_REGEX } from './jsx';
 export { kebabCase } from './kebab-case';
 export { buildSourceMap } from './source-maps';
 export { toBoolean } from './to-boolean';
