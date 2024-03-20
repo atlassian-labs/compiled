b2531bf3a2d6fcab7912a62e6bb016778534a8d7
Celeste <ccarloni@atlassian.com>
2024-03-20 15:27:42 +1000

    feat: add title attribute to Compiled style tag

--- packages/parcel-optimizer/src/index.ts
+++ packages/parcel-optimizer/src/index.ts
@@ -71,7 +71,7 @@ export default new Optimizer<ParcelOptimizerOpts, unknown>({
           .use(
             insertAt({
               selector: 'head',
-              append: '<style>' + stylesheet + '</style>',
+              append: '<style title="compiled">' + stylesheet + '</style>',
               behavior: 'inside',
             })
           )
