2e8369c60715d8fbdadf1348feed6e5e82da2f56
Grant Wong <gwong2@atlassian.com>
2024-07-15 17:27:51 +1000

    Implement GitHub action

--- .github/actions/task-checklist/index.js
+++ .github/actions/task-checklist/index.js
@@ -1,11 +1,32 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
+// Detects tasks in the form:
+// - [ ] Task 1
+//
+// Also handles whitespace around any of the characters,
+// and the three different ways to make a list (dash, asterisk, plus)
+const INCOMPLETE_TASKS_REGEX = /^\s*[-*+]\s+\[ \]\s+(.*)/g;
+
+const run = () => {
   const body = github.context.payload.pull_request?.body;
-  console.log(body);
-  console.log(`Hello world!`);
-} catch (error) {
-  // Handle errors and indicate failure
-  core.setFailed(error.message);
-}
+  if (!body) return;
+
+  const matches = body.matchAll(INCOMPLETE_TASKS_REGEX).map((match) => match[1]);
+
+  if (!matches.length) {
+    return;
+  }
+
+  for (const match of matches) {
+    console.log(`Found incomplete task: ${match}`);
+  }
+
+  core.setFailed(`
+Found an item in the PR description not marked as completed.
+
+Please complete this task before merging.
+`);
+};
+
+run();
