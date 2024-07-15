c7d3161cdd3b6b79de74d6474989ffe62ca9acd6
Grant Wong <gwong2@atlassian.com>
2024-07-15 17:09:43 +1000

    Set up dependencies, clean up code

--- .github/actions/task-checklist/index.js
+++ .github/actions/task-checklist/index.js
@@ -4,9 +4,7 @@ const github = require('@actions/github');
 try {
   const body = github.context.payload.pull_request?.body;
   console.log(body);
-  // Fetch the value of the input 'who-to-greet' specified in action.yml
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  console.log(`Hello world!`);
 } catch (error) {
   // Handle errors and indicate failure
   core.setFailed(error.message);
