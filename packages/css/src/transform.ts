0a8e4e97b4871e99ef640027dcba0c541bd56d26
Guilherme Oliveira <goliveira@atlassian.com>
2024-10-09 15:42:10 +1300

    feat: add prefix to generated hash rules

--- packages/css/src/transform.ts
+++ packages/css/src/transform.ts
@@ -20,6 +20,7 @@ export interface TransformOpts {
   increaseSpecificity?: boolean;
   sortAtRules?: boolean;
   sortShorthand?: boolean;
+  hashPrefix?: string;
 }
 
 /**
@@ -49,6 +50,7 @@ export const transformCss = (
       atomicifyRules({
         classNameCompressionMap: opts.classNameCompressionMap,
         callback: (className: string) => classNames.push(className),
+        hashPrefix: opts.hashPrefix,
       }),
       ...(opts.increaseSpecificity ? [increaseSpecificity()] : []),
       sortAtomicStyleSheet({
