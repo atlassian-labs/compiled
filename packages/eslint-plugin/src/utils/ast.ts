6b49bde67ea95fbbf7e51e3b35b743412a3a1d6d
Kylor Hall <khall2@atlassian.com>
2024-10-11 17:24:23 +1300

    Update default sources for 'find*LibraryImportDeclarations'

--- packages/eslint-plugin/src/utils/ast.ts
+++ packages/eslint-plugin/src/utils/ast.ts
@@ -1,4 +1,4 @@
-import { COMPILED_IMPORT } from '@compiled/utils';
+import { DEFAULT_IMPORT_SOURCES } from '@compiled/utils';
 import type { TSESTree, TSESLint } from '@typescript-eslint/utils';
 import type { Rule } from 'eslint';
 import type { ImportDeclaration, ImportSpecifier } from 'estree';
@@ -24,7 +24,7 @@ import type { ImportDeclaration, ImportSpecifier } from 'estree';
  */
 export const findLibraryImportDeclarations = (
   context: Rule.RuleContext,
-  sources = [COMPILED_IMPORT]
+  sources = DEFAULT_IMPORT_SOURCES
 ): ImportDeclaration[] => {
   return context
     .getSourceCode()
@@ -48,7 +48,7 @@ export const findLibraryImportDeclarations = (
  */
 export const findTSLibraryImportDeclarations = (
   context: TSESLint.RuleContext<string, readonly unknown[]>,
-  sources = [COMPILED_IMPORT]
+  sources = DEFAULT_IMPORT_SOURCES
 ): TSESTree.ImportDeclaration[] => {
   return context
     .getSourceCode()
