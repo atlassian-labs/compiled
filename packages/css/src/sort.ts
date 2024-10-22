60d50a514c32c4a075c19dd1b1e88d782652fc6d
Grant Wong <gwong2@atlassian.com>
2024-10-23 10:48:56 +1100

    Make the sort config optional

--- packages/css/src/sort.ts
+++ packages/css/src/sort.ts
@@ -8,14 +8,22 @@ import { sortAtomicStyleSheet } from './plugins/sort-atomic-style-sheet';
  * Sorts an atomic style sheet.
  *
  * @param stylesheet
- * @returns
+ * @returns the sorted stylesheet
  */
 export function sort(
   stylesheet: string,
   {
     sortAtRulesEnabled,
     sortShorthandEnabled,
-  }: { sortAtRulesEnabled: boolean | undefined; sortShorthandEnabled: boolean | undefined }
+  }: { sortAtRulesEnabled: boolean | undefined; sortShorthandEnabled: boolean | undefined } = {
+    // These default values should remain undefined so we don't override the default
+    // values set in packages/css/src/plugins/sort-atomic-style-sheet.ts
+    //
+    // Modify packages/css/src/plugins/sort-atomic-style-sheet.ts if you want to
+    // update the actual default values for sortAtRulesEnabled and sortShortEnabled.
+    sortAtRulesEnabled: undefined,
+    sortShorthandEnabled: undefined,
+  }
 ): string {
   const result = postcss([
     discardDuplicates(),
