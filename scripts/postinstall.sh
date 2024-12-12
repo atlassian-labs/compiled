ac422a3c322b1ee6765760a96c83ec2f1e959f1e
Grant Wong <gwong2@atlassian.com>
2024-12-12 11:08:50 +1100

    Update yarn to v4

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,19 +2,7 @@
 
 set -Eeuo pipefail
 
-trap cleanup SIGINT SIGTERM ERR EXIT
-
-cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-
-  rm -f yarn.lock.bak
-}
-
-# Remove any atlassian mirrors
-sed -i.bak -E 's/https:\/\/packages.atlassian.com\/api\/npm\/npm-remote\//https:\/\/registry.yarnpkg.com\//g' yarn.lock
+echo "De-duplicating..."
 
 # Deduplicate
-npx yarn-deduplicate
-
-# Run yarn again to transform any changes
-yarn --ignore-scripts
+yarn dedupe
