3d10a6d2bdc95b745a00b8fd01be43486e76df9a
Kylor Hall <136543114+kylorhall-atlassian@users.noreply.github.com>
2025-02-17 17:05:45 +1300

    Error on indirect dynamic styles and attempt to statically concatenate where feasible (#1795)
    * Fixes two scenarios found in https://github.com/atlassian-labs/compiled/issues/1789
    - The static syntax of `"8px ".concat("8px")` which is auto-generated by certain Babel targets generates `props.style` outputs when not necessary—we can simplify this to `"8px 8px"` ourselves.
    - Mixing "indirect selectors" (eg. `~`, `+`, `|`, `||`) that do not inherit CSS variables with dynamic styles which generates CSS variables and `props.style` will never work in Compiled, this now throws an error.
    
    * Add additional commentary, logic, type protection, and minor cleanup.
    
    * Update website
    
    * Remove snapshot

--- packages/babel-plugin/src/utils/object-property-to-string.ts
+++ packages/babel-plugin/src/utils/object-property-to-string.ts
@@ -48,7 +48,87 @@ const binaryExpressionToString = (
   throw new Error(`Cannot use ${operator} for string operation. Use + for string concatenation`);
 };
 
-const expressionToString: ExpressionToString = (expression, meta) => {
+type StringConcatExpression = t.Expression & {
+  callee: t.MemberExpression & {
+    computed: false;
+    object: t.StringLiteral;
+    property: t.Identifier & { name: 'concat' };
+  };
+  arguments: t.Expression[];
+};
+
+const isStringConcatExpression = (
+  expression: t.Expression
+): expression is StringConcatExpression => {
+  if (!t.isCallExpression(expression)) return false;
+  const callee = expression.callee;
+
+  if (
+    t.isMemberExpression(callee) &&
+    callee.computed === false &&
+    t.isStringLiteral(callee.object) &&
+    t.isIdentifier(callee.property) &&
+    callee.property.name === 'concat' &&
+    typeof expression.arguments?.length === 'number'
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+/**
+ * Determines if we think we can deeply and statically concatenate the arguments of a `"text ".concat('…')` call.
+ *
+ * For example, `"8px ".concat(' var(--ds-space-050)', ' var(--ds-space-100)')` might be output
+ * by Babel and statically concatenated to `"8px var(--ds-space-050) var(--ds-space-100)"`.
+ */
+export const canBeStaticallyConcatenated = (
+  expression: t.Expression
+): expression is StringConcatExpression & boolean => {
+  if (!isStringConcatExpression(expression)) return false;
+
+  const args = expression.arguments;
+  if (!args.length) return true;
+
+  // We only attempt to statically concatenate these specific arguments (though more types may be viable)
+  return args.every((arg) => {
+    // Example: `'b`' in `"a".concat('b', 'c')``
+    if (t.isStringLiteral(arg) || t.isNumericLiteral(arg)) return true;
+
+    // Example: `${b}` in `"a".concat(`${b}`, 'c')` where `b` is static, eg. `const b = 'b'`
+    if (t.isTemplateLiteral(arg)) return true;
+
+    return false;
+  });
+};
+
+/**
+ * Attempts to statically concatenate the arguments of a `"text ".concat('…')` call.
+ *
+ * For example, `"8px ".concat(' var(--ds-space-050)', ' var(--ds-space-100)')` might be output
+ * by Babel and this will statically concatenate it to `"8px var(--ds-space-050) var(--ds-space-100)"`.
+ */
+const concatToString = (
+  expression: StringConcatExpression,
+  meta: Metadata,
+  expressionToString: ExpressionToString
+): string => {
+  const callee = expression.callee;
+  const args = expression.arguments;
+  if (!args.length) return callee.object.value;
+
+  return args.reduce<string>((acc, arg) => {
+    if (!t.isExpression(arg)) {
+      // NOTE: We cannot concatenate this expression, however `canBeStaticallyConcatenated(…)` should catch this.
+      throw new Error(`Cannot concatenate an expression with non-expression arguments`);
+    }
+
+    return acc + expressionToString(arg, meta);
+  }, callee.object.value);
+};
+
+export const expressionToString: ExpressionToString = (expression, meta) => {
   // handles {'key-name': 'value'} or {1: 'value'}
   if (t.isStringLiteral(expression) || t.isNumericLiteral(expression)) {
     return String(expression.value);
@@ -73,6 +153,10 @@ const expressionToString: ExpressionToString = (expression, meta) => {
   // handles {['key-' + name]: 'value'}
   if (t.isBinaryExpression(expression)) {
     return binaryExpressionToString(expression, meta, expressionToString);
+  }
+
+  if (t.isCallExpression(expression) && canBeStaticallyConcatenated(expression)) {
+    return concatToString(expression, meta, expressionToString);
   }
 
   throw new Error(`${expression.type} has no name.'`);
