1583ba7f983336852234cbd701517f7fd1d8b212
Alex Reardon <areardon@atlassian.com>
2025-03-17 18:04:38 +1100

    updating test and bundle size values

--- packages/react/src/runtime/__perf__/ax.test.ts
+++ packages/react/src/runtime/__perf__/ax.test.ts
@@ -3,42 +3,61 @@ import { runBenchmark } from '@compiled/benchmark';
 import { ax } from '../index';
 
 describe('ax benchmark', () => {
-  const arr = [
-    '_19itglyw',
-    '_2rko1l7b',
-    '_ca0qftgi',
-    '_u5f319bv',
-    '_n3tdftgi',
-    '_19bv19bv',
-    '_bfhk1mzw',
-    '_syazu67f',
-    '_k48p1nn1',
-    '_ect41kw7',
-    '_1wybdlk8',
-    '_irr3mlcl',
-    '_1di6vctu',
-    // `undefined` is an acceptable parameter so we want to include it in the test case.
-    // Example: ax(['aaaabbbb', foo() && "aaaacccc"])
-    undefined,
+  const chunks: string[] = ['aaaa', 'bbbb', 'cccc', 'dddd', 'eeee', 'ffff', 'gggg'];
+  const uniques: string[] = chunks.map((chunk) => `_${chunk}${chunk}`);
+  const withClashes: string[] = [
+    ...Array.from({ length: 4 }, () => `_${chunks[0]}${chunks[0]}`),
+    ...Array.from({ length: 6 }, () => `_${chunks[0]}${chunks[1]}`),
+    ...Array.from({ length: 8 }, () => `_${chunks[0]}${chunks[2]}`),
   ];
 
-  it('completes with ax() string as the fastest', async () => {
-    // Remove undefined and join the strings
-    const str = arr.slice(0, -1).join(' ');
+  const getRandomRules = (() => {
+    function randomChunk() {
+      return chunks[Math.floor(Math.random() * chunks.length)];
+    }
 
+    return function create(): string[] {
+      return Array.from({ length: 20 }, () => `_${randomChunk()}${randomChunk()}`);
+    };
+  })();
+
+  it('completes with ax() string as the fastest', async () => {
     const benchmark = await runBenchmark('ax', [
       {
-        name: 'ax() array',
-        fn: () => ax(arr),
+        name: 'ax() single',
+        fn: () => ax(['_aaaabbbb']),
       },
       {
-        name: 'ax() string',
-        fn: () => ax([str, undefined]),
+        name: 'ax() uniques (array)',
+        fn: () => ax(uniques),
+      },
+      {
+        name: 'ax() uniques (as a string)',
+        fn: () => ax([uniques.join(' ')]),
+      },
+      {
+        name: 'ax() clashes',
+        fn: () => ax(withClashes),
+      },
+      {
+        name: 'ax() clashes (as a string)',
+        fn: () => ax([withClashes.join(' ')]),
+      },
+      {
+        name: 'ax() random keys (no clashes)',
+        fn: () => ax(getRandomRules()),
+      },
+      {
+        name: 'ax() random keys (with clashes)',
+        fn: () => {
+          const random = getRandomRules();
+          ax([...random, ...random, ...random]);
+        },
       },
     ]);
 
     expect(benchmark).toMatchObject({
-      fastest: ['ax() string'],
+      fastest: ['ax() single'],
     });
-  }, 30000);
+  }, 90000);
 });
