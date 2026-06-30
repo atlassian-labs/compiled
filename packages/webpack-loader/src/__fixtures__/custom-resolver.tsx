f0eda8d1691d9da41ba7002614d93d9190414565
Kylor Hall <136543114+kylorhall-atlassian@users.noreply.github.com>
2026-07-01 11:36:25 +1200

    Apply suggestions from code review
    Co-authored-by: Kylor Hall <136543114+kylorhall-atlassian@users.noreply.github.com>

--- packages/webpack-loader/src/__fixtures__/custom-resolver.tsx
+++ packages/webpack-loader/src/__fixtures__/custom-resolver.tsx
@@ -1,8 +1,6 @@
 /** @jsx jsx */
 import { jsx, css } from '@compiled/react';
 import type { JSX } from 'react';
-// @ts-ignore This is a bug where the meta is not updated for the new file import
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 // @ts-expect-error
 import { primary } from 'test';
 
--- packages/webpack-loader/src/__fixtures__/webpack-alias.tsx
+++ packages/webpack-loader/src/__fixtures__/webpack-alias.tsx
@@ -1,8 +1,7 @@
 /** @jsx jsx */
 // @ts-expect-error This is a bug where the meta is not updated for the new file import
-import { jsx, css } from '@compiled/react';
+import { jsx } from '@compiled/react';
 import type { JSX } from 'react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 // @ts-expect-error
 import { styles } from 'webpack-alias';
 
